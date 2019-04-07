39847d09c2ccb0311cf15620b190d9879bb85ad7
topskilldev <me@topskill.dev>
2019-04-07 19:37:53 +0100

    Added comment to ErrorBoundary component

--- src/hoc/ErrorBoundary.js
+++ src/hoc/ErrorBoundary.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
 
+/*
+
+This component renders whenever something goes wrong in production. Consider 
+using a component that renders a more appealing message 
+
+*/
 export default class ErrorBoundary extends Component {
   state = {
     error: null
@@ -16,7 +22,6 @@ export default class ErrorBoundary extends Component {
       return (
         <div>
           <h2 style={{ textAlign: "center" }}>
-            {" "}
             Something went wrong. We're working on it
           </h2>
         </div>
