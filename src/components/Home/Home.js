8f68b78fdf4acbd0dd893ee5df7092cb9b0dd22a
topskilldev <me@topskill.dev>
2019-04-08 10:53:54 +0100

    Added new comment to Home Component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,14 @@ import React from "react";
 import Header from "../Header/Header";
 import Hero from "../Hero/Hero";
 
+/*
+
+This is the Landing Page for the App. Our current approach merges components
+into the same folder. We'll eventually move stuff around so it's easier to 
+reason about in the end
+
+*/
+
 export default function Home() {
   return (
     <div>
