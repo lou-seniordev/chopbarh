10a6a4b75f859b7fb54f40ef1238b9d4994dd4fb
topskilldev <me@topskill.dev>
2019-04-08 13:23:58 +0100

    Added media queries js file

---
+++ src/components/styles/breakpoints.js
@@ -0,0 +1,9 @@
+export default {
+  largest: "75em", // 1200px
+  large: "62.5em", // 1000px
+  medium: "50em", // 800px;
+  mediumLite: "48rem", // 768px;
+  small: "36em", // 576px;
+  smaller: "29em", // 464px;
+  smallest: "25em" // 400px;
+};
