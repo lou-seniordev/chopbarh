4bffb6ccc3007140952cd5c75f0b3c52941df4d8
topskilldev <me@topskill.dev>
2019-04-08 10:45:37 +0100

    Added margin to Hero Description

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,24 +1,90 @@
 import React from "react";
+import styled from "styled-components";
+import color from "../styles/colors";
+
+const HeroWrapper = styled.div`
+  height: 85vh;
+  padding: 3rem 1rem;
+  position: relative;
+  background: ${color.colorPrimary};
+  color: ${color.colorWhite};
+
+  &__image {
+    width: 60rem;
+    position: absolute;
+    top: 60%;
+    /* transform: translateY(-50%); */
+  }
+
+  &__content {
+    /* @media only screen and (max-width: $bp-medium) {
+      top: 40%;
+      left: 50%;
+      transform: translateX(-50%);
+    } */
+  }
+`;
+
+const HeroContentWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 10%;
+  transform: translateY(-50%);
+`;
+
+const HeadingTwo = styled.h2`
+  font-size: 7rem;
+  text-transform: uppercase;
+  font-style: italic;
+  font-weight: bold;
+`;
+
+const ParagraphOne = styled.p`
+  font-size: 1.5rem;
+  font-weight: 400;
+`;
+
+const Button = styled.button`
+  all: unset;
+  border: 3px solid ${color.colorWhite};
+  padding: 1rem 1.3rem;
+  font-weight: 600;
+  transform: skew(-20deg);
+  display: inline-block;
+  transition: all 0.2s;
+
+  span {
+    display: inline-block;
+    transform: skew(20deg);
+  }
+
+  &:hover {
+    transform: translateY(-3px) skew(-20deg);
+    color: ${color.colorWhite};
+  }
+`;
 
 export default function Hero() {
   return (
-    <div>
-      <div class="hero__content">
-        <img src="assets/img/Smiling guy.png" class="hero__image" alt="" />
-        <h2 class="hero__title">Bet.Play.Win.</h2>
-        <div class="hero__description">
-          Play and win from collection of childhood games that live up to the
-          moment
+    <HeroWrapper>
+      <HeroContentWrapper>
+        <img src="assets/img/Smiling guy.png" className="hero__image" alt="" />
+        <HeadingTwo className="hero__title">Bet.Play.Win.</HeadingTwo>
+        <div className="mt-n4">
+          <ParagraphOne>
+            Play and win from collection of childhood games that live up to the
+            moment
+          </ParagraphOne>
         </div>
-        <div class="hero__buttons mt-5">
-          <button class="hero__button mr-4">
-            <span>Google Play</span>
-          </button>
-          <button class="hero__button">
-            <span>iOS Store</span>
-          </button>
+        <div className="hero__buttons mt-3">
+          <Button className="mr-4">
+            <span>Start Playing</span>
+          </Button>
+          <Button className="ml-4">
+            <span>Download</span>
+          </Button>
         </div>
-      </div>
-    </div>
+      </HeroContentWrapper>
+    </HeroWrapper>
   );
 }
