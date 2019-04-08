fc2c803b866caf39b3d57f1f25de9249d05007b0
topskilldev <me@topskill.dev>
2019-04-08 11:12:15 +0100

    Added wrapper to HowItWorks component

--- src/components/HowItWorks/HowItWorks.js
+++ src/components/HowItWorks/HowItWorks.js
@@ -1,42 +1,62 @@
 import React from "react";
+import styled from "styled-components";
+
+const HowItWorksWrapper = styled.div`
+  text-align: center;
+  padding: 2rem;
+`;
+
+const ContentBlock = styled.div`
+  max-width: 32rem;
+  min-height: 22rem;
+  background: #aca3a3;
+  transform: skew(-8deg);
+  padding: 1rem;
+  display: flex;
+  background: peru;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default function HowItWorks() {
   return (
-    <div>
-      <h3 className="how-it-works__shell-header mb-5">How it Works</h3>
-      <div className="row mb-5">
-        <div className="col-md-4 mb-2">
-          <div className="works__content">
-            <div className="works__content--container">
-              <img src="assets/svg/Game.svg" className="mb-4" alt="" />
-              <h4 className="text-uppercase">Choose your Game</h4>
-              <p className="p--2">Choose your game you love</p>
-            </div>
+    <HowItWorksWrapper>
+      <div className="container">
+        <h3 className="mb-5">How it Works</h3>
+        <div className="row mb-5">
+          <div className="col-md-4 mb-2">
+            <ContentBlock>
+              <div>
+                <img src="assets/svg/Game.svg" className="mb-4" alt="" />
+                <h4 className="text-uppercase">Choose your Game</h4>
+                <p className="p--2">Choose your game you love</p>
+              </div>
+            </ContentBlock>
           </div>
-        </div>
-        <div className="col-md-4 mb-2">
-          <div className="works__content">
-            <div className="works__content--container">
-              <img src="assets/svg/Bet away.svg" className="mb-3" alt="" />
-              <h4 className="text-uppercase">Bet Away!</h4>
-              <p className="p--2">Put in the amount you want to bet</p>
-            </div>
+          <div className="col-md-4 mb-2">
+            <ContentBlock>
+              <div>
+                <img src="assets/svg/Bet away.svg" className="mb-3" alt="" />
+                <h4 className="text-uppercase">Bet Away!</h4>
+                <p className="p--2">Put in the amount you want to bet</p>
+              </div>
+            </ContentBlock>
           </div>
-        </div>
-        <div className="col-md-4 mb-2">
-          <div className="works__content">
-            <div className="works__content--container">
-              <img
-                src="assets/svg/Versus and win.svg"
-                className="mb-4"
-                alt=""
-              />
-              <h4 className="text-uppercase">Play and Win!</h4>
-              <p className="p--2">Win as much as you can!</p>
-            </div>
+          <div className="col-md-4 mb-2">
+            <ContentBlock>
+              <div>
+                <img
+                  src="assets/svg/Versus and win.svg"
+                  className="mb-4"
+                  alt=""
+                />
+                <h4 className="text-uppercase">Play and Win!</h4>
+                <p className="p--2">Win as much as you can!</p>
+              </div>
+            </ContentBlock>
           </div>
         </div>
       </div>
-    </div>
+    </HowItWorksWrapper>
   );
 }
