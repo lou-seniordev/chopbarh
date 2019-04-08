7ec6dbab4890352da7f8550e0c9c585d222bdb95
topskilldev <me@topskill.dev>
2019-04-08 11:23:04 +0100

    Renamed svg assets

--- src/components/HowItWorks/HowItWorks.js
+++ src/components/HowItWorks/HowItWorks.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import color from "../styles/colors";
-
 const HowItWorksWrapper = styled.div`
   text-align: center;
   padding: 2rem;
