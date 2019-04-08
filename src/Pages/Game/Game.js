55b33616fe6ab2cc05940e254c2ca4ad1078c2ed
topskilldev <me@topskill.dev>
2019-04-08 16:21:06 +0100

    Added Games Page

---
+++ src/Pages/Game/Game.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+/*
+
+The components in this folder will be rendered as the Pages for the 
+different Routes in Layout
+
+*/
+
+export default function Game() {
+  return <div />;
+}
