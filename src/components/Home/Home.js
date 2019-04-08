7318dd7be63008e8e450a41a40e7c502acbd5ee3
topskilldev <me@topskill.dev>
2019-04-08 09:05:55 +0100

    Added colors file to styles

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Header</p>
+      <p>Hero</p>
+      <p>Content</p>
+      <p>Footer</p>
+    </div>
+  );
+}
