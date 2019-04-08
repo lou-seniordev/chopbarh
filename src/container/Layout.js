a86f396351591b5db3994b6f68dfb92c371a35b1
topskilldev <me@topskill.dev>
2019-04-08 09:01:09 +0100

    Added Global Styles with styled-components

--- src/container/Layout.js
+++ src/container/Layout.js
@@ -1,7 +1,32 @@
 import React, { Component } from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  *, 
+  *:after, 
+  *:before {
+    padding: 0;
+    margin: 0;
+    box-sizing:inherit;
+  }
+
+  html {
+    box-sizing: border-box;
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: 'Montserrat', sans-serif;
+  }
+`;
 
 export default class Layout extends Component {
   render() {
-    return <div />;
+    return (
+      <>
+        <GlobalStyles />
+        <div />
+      </>
+    );
   }
 }
