2bd8a45e35f97f1fa4b7c19ec5b200fa10c3d2fc
topskilldev <me@topskill.dev>
2019-04-08 12:49:48 +0100

    Added base Footer Logic

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,68 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer class="footer">
+      <div class="footer__container">
+        <div class="">
+          <img src="assets/img/Chopbarr@2x.png" alt="" srcset="" />
+          <p>Paragraph</p>
+        </div>
+        <div class="">
+          <div class="footer-link__header">
+            <p>Chopbarh</p>
+          </div>
+          <div class="footer-link__item-container">
+            <p>
+              <a href="">About Us</a>
+            </p>
+            <p>
+              <a href="">Terms and Conditions</a>
+            </p>
+            <p>
+              <a href="">Privacy Policy</a>
+            </p>
+            <p>
+              <a href="">Responsible Gambling</a>
+            </p>
+            <p>
+              <a href="">Become a Partner</a>
+            </p>
+          </div>
+        </div>
+        <div class="">
+          <div class="footer-link__header">
+            <p>Gaming</p>
+          </div>
+          <div class="footer-link__item-container">
+            <p>
+              <a href="">How to Play</a>
+            </p>
+            <p>
+              <a href="">Games</a>
+            </p>
+            <p>
+              <a href="">Betting</a>
+            </p>
+            <p>
+              <a href="">Download</a>
+            </p>
+            <p>
+              <a href="">Talk to us</a>
+            </p>
+          </div>
+        </div>
+        <div class="align-self-start">
+          <div class="footer-link__header">
+            <p>Contact Us</p>
+          </div>
+          <div class="footer-link__item-container">
+            <p>Telephone: 09038764982</p>
+            <p>Email: help@chopbarh.com</p>
+            <p>&copy; 2019</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
