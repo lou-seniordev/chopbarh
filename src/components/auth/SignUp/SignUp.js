912c6bb691e63f6b10dfd2c31f83611b0d69fbb9
topskilldev <me@topskill.dev>
2019-04-09 11:07:22 +0100

    Added new comment to SignUp

--- src/components/auth/SignUp/SignUp.js
+++ src/components/auth/SignUp/SignUp.js
@@ -2,23 +2,45 @@ import React from "react";
 import styled from "styled-components";
 import color from "../../styles/colors";
 import Header from "../../UI/Header/Header";
-import Background from "../../assets/svg/WavyHeader.svg";
+//import Background from "../../assets/svg/WavyHeader.svg";
 
+/*
+
+Swap out the border thing for some Box Shadow
+
+*/
 const SignUpWrapper = styled.div`
-  height: 30vh;
+  height: 50vh;
+  background: ${color.colorPrimary};
+  margin-top: -10rem;
+  z-index: 2000;
+  padding: 10rem;
+
+  div {
+    display: flex;
+    justify-content: center;
+    margin: 10rem auto;
+    width: 60vw;
+    background: #fff;
+    padding: 2rem;
+    border: 1px solid gray;
+  }
 `;
 
-const Image = styled.img`
-  width: 100%;
-  margin-top: -8rem;
-`;
+// const Image = styled.img`
+//   width: 100%;
+//   height: 10%;
+//   margin-top: -8rem;
+// `;
 
 export default function SignUp() {
   return (
     <>
       <Header transparent />
-      <Image src={Background} alt="Background" />
-      <SignUpWrapper />
+      {/* <Image src={Background} alt="Background" /> */}
+      <SignUpWrapper>
+        <div>ITEM HERE</div>
+      </SignUpWrapper>
     </>
   );
 }
