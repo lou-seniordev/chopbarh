bc90d2c0563127de2b86490c309506191fc5e51d
topskilldev <me@topskill.dev>
2019-04-09 03:57:53 +0100

    Added GameItems to Games component

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../UI/Header/Header";
 import GamesHero from "./GamesHero/GamesHero";
+import GameItems from "./GameItems/GameItems";
 
 export default function Games() {
   return (
     <>
       <Header transparent />
       <GamesHero />
+      <GameItems />
     </>
   );
 }
