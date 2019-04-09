2c92ed6a3a2bdfec5c60b2138d56546f1f98d218
topskilldev <me@topskill.dev>
2019-04-09 16:54:02 +0100

    Adjusted x-margin for Icon component

--- src/components/Dashboard/shared/Icon/Icon.js
+++ src/components/Dashboard/shared/Icon/Icon.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Icon({ icon }) {
-  return <img src={icon} className="mr-3" alt="Icon" width="30" />;
+  return <img src={icon} className="mx-3" alt="Icon" width="30" />;
 }
