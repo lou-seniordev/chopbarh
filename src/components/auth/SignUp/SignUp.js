722302d4932cf6bbfc41c3a84e2accc9e870e264
topskilldev <me@topskill.dev>
2019-04-09 10:29:07 +0100

    Added Background image to SignUp component

--- src/components/auth/SignUp/SignUp.js
+++ src/components/auth/SignUp/SignUp.js
@@ -2,15 +2,22 @@ import React from "react";
 import styled from "styled-components";
 import color from "../../styles/colors";
 import Header from "../../UI/Header/Header";
+import Background from "../../assets/svg/WavyHeader.svg";
 
 const SignUpWrapper = styled.div`
-  background: ${color.colorPrimary};
+  height: 30vh;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  margin-top: -8rem;
 `;
 
 export default function SignUp() {
   return (
     <>
       <Header transparent />
+      <Image src={Background} alt="Background" />
       <SignUpWrapper />
     </>
   );
