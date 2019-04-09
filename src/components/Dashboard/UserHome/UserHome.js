c48f45c22b0886daef1194f6054700771a1f1531
topskilldev <me@topskill.dev>
2019-04-09 17:05:28 +0100

    Added Footer to UserHome component

--- src/components/Dashboard/UserHome/UserHome.js
+++ src/components/Dashboard/UserHome/UserHome.js
@@ -1,10 +1,12 @@
 import React from "react";
 import UserHeader from "../shared/UserHeader/UserHeader";
+import Footer from "../../UI/Footer/Footer";
 
 export default function UserHome() {
   return (
     <>
       <UserHeader />
+      <Footer />
     </>
   );
 }
