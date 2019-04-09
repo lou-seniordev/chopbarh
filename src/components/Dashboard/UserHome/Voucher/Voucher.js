3cc589fd2d61bd05396d14c72829cad92fa752b0
topskilldev <me@topskill.dev>
2019-04-09 20:12:50 +0100

    Changed Button text

--- src/components/Dashboard/UserHome/Voucher/Voucher.js
+++ src/components/Dashboard/UserHome/Voucher/Voucher.js
@@ -1,15 +1,93 @@
 import React from "react";
 import styled from "styled-components";
+import color from "../../../styles/colors";
+import breakPoints from "../../../styles/breakpoints";
 
 const VoucherWrapper = styled.div`
   margin-top: 6rem;
+`;
+
+const FormWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  button {
+    all: unset;
+    padding: 0.5rem 1.3rem;
+    transform: skew(-20deg);
+    display: inline-block;
+    transition: all 0.2s;
+    color: ${color.colorWhite};
+    background: ${color.colorPrimary};
+    font-size: 1.3rem;
+    z-index: 200;
+
+    /* @media only screen and (max-width: ${breakPoints.mediumLite}) {
+      align-self: flex-start;
+      padding: 0.5rem 1.7rem;
+      margin-bottom: 0.5rem;
+      margin-top: 0.5rem;
+    } */
+
+    span {
+      display: inline-block;
+      transform: skew(20deg);
+      color: #fff;
+    }
+
+    &:hover {
+      transform: translateY(-3px) skew(-20deg);
+      background: ${color.colorPrimaryHover};
+      color: ${color.colorWhite};
+    }
+  }
+`;
+
+const FormItem = styled.div`
+  label {
+    font-size: 1.4rem;
+    font-weight: 600;
+    color: #737773;
+    margin-bottom: 1rem;
+  }
+
+  input {
+    color: #8d8e8d;
+    width: 30rem;
+    height: 3.4rem;
+    margin-bottom: 2rem;
+    border: 0;
+    background: #f6f6f6;
+    outline: none;
+    padding: 3px 5px;
+  }
+
+  & > * {
+    display: block;
+    font-family: inherit;
+  }
 `;
 
 export default function Voucher() {
   return (
     <VoucherWrapper className="container">
       <div className="row">
-        <div className="col-md-6">Play Voucher</div>
+        <div className="col-md-6">
+          <FormWrapper>
+            <form>
+              <FormItem>
+                <label>Load Voucher</label>
+              </FormItem>
+              <FormItem>
+                <input type="text" placeholder="Voucher Code" />
+              </FormItem>
+              <button type="submit" className="mr-2">
+                <span>Load</span>
+              </button>
+            </form>
+          </FormWrapper>
+        </div>
         <div className="col-md-6">
           <table class="table table-striped">
             <thead style={{ background: "#8C1936", color: "#fff" }}>
