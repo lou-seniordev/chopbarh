e7251588b3d408c6c91379c88d69f408e7559d6e
topskilldev <me@topskill.dev>
2019-04-09 05:08:40 +0100

    Added AuthWrapper styles

--- src/components/auth/Login/Login.js
+++ src/components/auth/Login/Login.js
@@ -1,5 +1,34 @@
 import React from "react";
+import styled from "styled-components";
+import breakPoints from "../../styles/breakpoints";
+
+const AuthWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 100vh;
+
+  @media only screen and (max-width: ${breakPoints.mediumLite}) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+const ImageContainer = styled.div`
+  background: pink;
+
+  @media only screen and (max-width: ${breakPoints.mediumLite}) {
+    display: none;
+  }
+`;
+
+const FormWrapper = styled.div`
+  background: peru;
+`;
 
 export default function Login() {
-  return <div>p</div>;
+  return (
+    <AuthWrapper>
+      <ImageContainer>IMAGE</ImageContainer>
+      <FormWrapper>FORM</FormWrapper>
+    </AuthWrapper>
+  );
 }
