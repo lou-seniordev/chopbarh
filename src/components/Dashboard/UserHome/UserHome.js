1a9df32277e9d922f55c6376b808787208974705
topskilldev <me@topskill.dev>
2019-04-09 17:15:18 +0100

    Added UserNavigation to UserHome component

--- src/components/Dashboard/UserHome/UserHome.js
+++ src/components/Dashboard/UserHome/UserHome.js
@@ -1,11 +1,13 @@
 import React from "react";
 import UserHeader from "../shared/UserHeader/UserHeader";
 import Footer from "../../UI/Footer/Footer";
+import UserNavigation from "../shared/UserNavigation/UserNavigation";
 
 export default function UserHome() {
   return (
     <>
       <UserHeader />
+      <UserNavigation />
       <Footer />
     </>
   );
