d75bf841c804741a816ee58ef3fa2685c19ce3df
topskilldev <me@topskill.dev>
2019-04-09 04:21:56 +0100

    Added button to GameItems

--- src/components/Games/GameItems/GameItems.js
+++ src/components/Games/GameItems/GameItems.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import color from "../../styles/colors";
+import breakPoints from "../../styles/breakpoints";
 import Ludo from "../../assets/img/Ludo@2x.png";
 import Whot from "../../assets/img/Whot.png";
 
@@ -30,6 +31,36 @@ const Column = styled.div`
   }
 `;
 
+const Button = styled.button`
+  all: unset;
+  border: 3px solid ${color.colorWhite};
+  padding: 1rem 1.3rem;
+  font-size: 1.5rem;
+  font-weight: 600;
+  transform: skew(-20deg);
+  display: inline-block;
+  transition: all 0.2s;
+
+  span {
+    display: inline-block;
+    transform: skew(20deg);
+  }
+
+  &:hover {
+    transform: translateY(-3px) skew(-20deg);
+    color: ${color.colorWhite};
+    background: ${color.colorPrimaryHover};
+  }
+
+  @media only screen and (max-width: ${breakPoints.medium}) {
+    font-size: 1.3rem;
+  }
+
+  @media only screen and (max-width: ${breakPoints.small}) {
+    font-size: 1.2rem;
+  }
+`;
+
 export default function GameItems() {
   return (
     <GameItemsWrapper>
@@ -41,10 +72,22 @@ export default function GameItems() {
             <p>
               Play and win from childhood collections that live up to the moment
             </p>
+            <Button>
+              <span>Place Bet</span>
+            </Button>
+            <Button>
+              <span>Google Play</span>
+            </Button>
+            <Button>
+              <span>iOS Store</span>
+            </Button>
           </Column>
           <Column className="col-md-6">
             <Image alt="Whot" src={Whot} />
             <HeadingThree>Whot</HeadingThree>
+            <p>
+              Play and win from childhood collections that live up to the moment
+            </p>
           </Column>
         </div>
       </div>
