4673ebd3879e59c58d10ce43b0549d7778716796
topskilldev <me@topskill.dev>
2019-04-09 10:19:21 +0100

    Added base logic to SignUp component

---
+++ src/components/auth/SignUp/SignUp.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "../../UI/Header/Header";
+
+const SignUpWrapper = styled.div``;
+
+export default function SignUp() {
+  return (
+    <SignUpWrapper>
+      <Header transparent />
+    </SignUpWrapper>
+  );
+}
