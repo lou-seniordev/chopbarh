b3cc1fca82ef05bec96e52be6b89467713419e0f
topskilldev <me@topskill.dev>
2019-04-09 13:46:19 +0100

    Changed AnimatedSwitch component

--- src/container/Layout.js
+++ src/container/Layout.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { createGlobalStyle } from "styled-components";
 import { Switch, Route, Redirect } from "react-router-dom";
-import { spring, AnimatedRoute } from "react-router-transition";
 // import ErrorBoundary from "../hoc/ErrorBoundary";
 import color from "../components/styles/colors";
 import Home from "../components/Home/Home";
@@ -9,43 +8,6 @@ import Games from "../components/Games/Games";
 import Login from "../components/auth/Login/Login";
 import ForgotPassword from "../components/auth/ForgotPassword/ForgotPassword";
 import SignUp from "../components/auth/SignUp/SignUp";
-import "./Layout.css";
-
-// we need to map the `scale` prop we define below
-// to the transform style property
-function mapStyles(styles) {
-  return {
-    opacity: styles.opacity,
-    transform: `scale(${styles.scale})`
-  };
-}
-
-// wrap the `spring` helper to use a bouncy config
-function bounce(val) {
-  return spring(val, {
-    stiffness: 330,
-    damping: 22
-  });
-}
-
-// child matches will...
-const bounceTransition = {
-  // start in a transparent, upscaled state
-  atEnter: {
-    opacity: 0,
-    scale: 1.1
-  },
-  // leave in a transparent, downscaled state
-  atLeave: {
-    opacity: bounce(0),
-    scale: bounce(0.5)
-  },
-  // and rest at an opaque, normally-scaled state
-  atActive: {
-    opacity: bounce(1),
-    scale: bounce(1)
-  }
-};
 
 /*
 
@@ -85,20 +47,14 @@ export default class Layout extends Component {
     return (
       <>
         <GlobalStyles />
-        <AnimatedSwitch
-          atEnter={bounceTransition.atEnter}
-          atLeave={bounceTransition.atLeave}
-          atActive={bounceTransition.atActive}
-          mapStyles={mapStyles}
-          className="route-wrapper"
-        >
+        <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/games" component={Games} />
           <Route path="/login" component={Login} />
           <Route path="/reset" component={ForgotPassword} />
           <Route path="/signup" component={SignUp} />
           <Redirect to="/" />
-        </AnimatedSwitch>
+        </Switch>
       </>
     );
   }
