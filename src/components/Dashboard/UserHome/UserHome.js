a74e7ea3ebe52f448e02a6a045c8bdb050e98df7
topskilldev <me@topskill.dev>
2019-04-09 18:45:11 +0100

    Added QuickPlay component to UserHome

--- src/components/Dashboard/UserHome/UserHome.js
+++ src/components/Dashboard/UserHome/UserHome.js
@@ -2,12 +2,14 @@ import React from "react";
 import UserHeader from "../shared/UserHeader/UserHeader";
 import Footer from "../../UI/Footer/Footer";
 import UserNavigation from "../shared/UserNavigation/UserNavigation";
+import QuickPlay from "./QuickPlay/QuickPlay";
 
 export default function UserHome() {
   return (
     <>
       <UserHeader />
       <UserNavigation />
+      <QuickPlay />
       <Footer />
     </>
   );
