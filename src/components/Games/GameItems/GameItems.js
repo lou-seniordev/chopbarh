f35900b7df222ec31b15066ba391628cb2683343
topskilldev <me@topskill.dev>
2019-04-09 04:18:31 +0100

    Added Column block to GameItems

--- src/components/Games/GameItems/GameItems.js
+++ src/components/Games/GameItems/GameItems.js
@@ -1,9 +1,33 @@
 import React from "react";
 import styled from "styled-components";
+import color from "../../styles/colors";
 import Ludo from "../../assets/img/Ludo@2x.png";
+import Whot from "../../assets/img/Whot.png";
 
 const GameItemsWrapper = styled.div`
   margin-top: 5rem;
+`;
+
+const Image = styled.img``;
+
+const HeadingThree = styled.h3`
+  font-size: 3rem;
+  text-transform: uppercase;
+  font-style: italic;
+  font-weight: bold;
+  color: ${color.colorPrimary};
+`;
+
+const Column = styled.div`
+  text-align: center;
+
+  p {
+    text-align: left;
+    width: 75%;
+    color: #c5c7c5;
+    font-size: 1.5rem;
+    margin: 0 auto;
+  }
 `;
 
 export default function GameItems() {
@@ -11,10 +35,17 @@ export default function GameItems() {
     <GameItemsWrapper>
       <div className="container">
         <div className="row">
-          <div className="col-md-6">
-            <img alt="Ludo" src={Ludo} />
-          </div>
-          <div className="col-md-6">TABLE SOCCER</div>
+          <Column className="col-md-6">
+            <Image alt="Ludo" src={Ludo} />
+            <HeadingThree>Ludo</HeadingThree>
+            <p>
+              Play and win from childhood collections that live up to the moment
+            </p>
+          </Column>
+          <Column className="col-md-6">
+            <Image alt="Whot" src={Whot} />
+            <HeadingThree>Whot</HeadingThree>
+          </Column>
         </div>
       </div>
     </GameItemsWrapper>
