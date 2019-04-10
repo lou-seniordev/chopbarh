894b536e4657764520926373447a2f42e62a0b68
topskilldev <me@topskill.dev>
2019-04-10 15:16:43 +0100

    Removed waves from Home Page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import styled from "styled-components";
 import Header from "../UI/Header/Header";
 import Hero from "./Hero/Hero";
 import HowItWorks from "./HowItWorks/HowItWorks";
@@ -7,15 +6,6 @@ import PlayAndChop from "./PlayAndChop/PlayAndChop";
 import Testimonials from "./Testimonials/Testimonials";
 import GameList from "./GameList/GameList";
 import Footer from "../UI/Footer/Footer";
-import Background from "../assets/svg/WavyBackground.svg";
-
-const BackgroundImage = styled.div`
-  background: url(${Background});
-  height: auto;
-  max-width: 99.2vw;
-  margin-top: -7rem;
-  padding-top: 9rem;
-`;
 
 /*
 
@@ -30,12 +20,10 @@ export default function Home() {
     <>
       <Header />
       <Hero />
-      <BackgroundImage>
-        <HowItWorks />
-        <PlayAndChop />
-        <Testimonials />
-        <GameList />
-      </BackgroundImage>
+      <HowItWorks />
+      <PlayAndChop />
+      <Testimonials />
+      <GameList />
       <Footer />
     </>
   );
