11229a677848eeb7c239786cd50cba067c241d42
topskilldev <me@topskill.dev>
2019-04-10 13:55:00 +0100

    Added Logic to EditProfileForm component

---
+++ src/components/Dashboard/UserProfile/EditProfileForm/EditProfileForm.js
@@ -0,0 +1,174 @@
+import React from "react";
+import styled from "styled-components";
+import color from "../../styles/colors";
+import breakPoints from "../../styles/breakpoints";
+
+const SignUpWrapper = styled.div`
+  height: 60vh;
+  background: ${color.colorPrimary};
+  margin-top: -26rem;
+  z-index: 2000;
+  padding: 20rem 10rem;
+
+  @media only screen and (max-width: ${breakPoints.small}) {
+    padding: 20rem 5rem;
+  }
+
+  @media only screen and (max-width: ${breakPoints.smaller}) {
+    padding: 20rem 0rem;
+  }
+`;
+
+const Container = styled.div`
+  margin: 10rem auto;
+  width: 60vw;
+  background: #fff;
+  padding: 4rem 10rem;
+  border: 1px solid gray;
+
+  @media only screen and (max-width: ${breakPoints.mediumLite}) {
+    padding: 4rem 5rem;
+  }
+
+  @media only screen and (max-width: ${breakPoints.small}) {
+    width: 80vw;
+    padding: 4rem 1rem;
+  }
+`;
+
+const HeadingTwo = styled.h2`
+  font-size: 2.5rem;
+  color: ${color.colorPrimary};
+  /* color: #c5c7c5; */
+  font-weight: bold;
+  text-align: center;
+`;
+
+const Form = styled.form`
+  position: relative;
+
+  button {
+    all: unset;
+    padding: 0.5rem 1.3rem;
+    display: inline-block;
+    position: absolute;
+    left: 50%;
+    transform: skew(-20deg) translateX(-50%);
+    transition: all 0.2s;
+    color: ${color.colorWhite};
+    background: ${color.colorPrimary};
+    font-size: 1.3rem;
+    z-index: 200;
+
+    @media only screen and (max-width: ${breakPoints.mediumLite}) {
+      font-size: 1.1rem;
+    }
+
+    span {
+      display: inline-block;
+      transform: skew(20deg);
+      color: #fff;
+    }
+
+    &:hover {
+      transform: translateY(-3px) skew(-20deg) translateX(-50%);
+      background: ${color.colorPrimaryHover};
+      color: ${color.colorWhite};
+    }
+  }
+`;
+
+const FormItem = styled.div`
+  label {
+    font-size: 1.4rem;
+    font-weight: 600;
+    color: #737773;
+    margin-bottom: 1rem;
+  }
+
+  input,
+  select {
+    color: #8d8e8d;
+    width: 100%;
+    height: 3.4rem;
+    margin-bottom: 2rem;
+    border: 0;
+    background: #f6f6f6;
+    outline: none;
+    padding: 3px 5px;
+  }
+
+  & > * {
+    display: block;
+    font-family: inherit;
+  }
+`;
+
+const HalfColumn = styled.div`
+  display: flex;
+
+  @media only screen and (max-width: ${breakPoints.large}) {
+    flex-direction: column;
+  }
+
+  div {
+    width: 50%;
+
+    @media only screen and (max-width: ${breakPoints.large}) {
+      width: 100%;
+    }
+  }
+`;
+
+export default function EditProfileForm() {
+  return (
+    <>
+      <SignUpWrapper>
+        <Container>
+          <Form>
+            <HeadingTwo className="mb-4">Sign Up</HeadingTwo>
+            <FormItem>
+              <label>Full Name</label>
+              <input type="text" required />
+            </FormItem>
+            <FormItem>
+              <label>Phone Number</label>
+              <input type="tel" required />
+            </FormItem>
+            <HalfColumn>
+              <FormItem className="mr-3">
+                <label>Date of Birth</label>
+                <input type="date" required />
+              </FormItem>
+              <FormItem>
+                <label>Sex</label>
+                <select>
+                  <option>Male</option>
+                  <option>Female</option>
+                  <option>Other</option>
+                </select>
+              </FormItem>
+            </HalfColumn>
+            <HalfColumn>
+              <FormItem className="mr-3">
+                <label>Enter Pin (4 to 6 digits)</label>
+                <input type="password" required minlength="4" maxlength="6" />
+              </FormItem>
+              <FormItem>
+                <label>Re-enter Pin</label>
+                <input type="password" required minlength="4" maxlength="6" />
+              </FormItem>
+            </HalfColumn>
+            <FormItem>
+              <label>Email</label>
+              <input type="email" required />
+            </FormItem>
+            <button type="submit" className="mr-2">
+              <span>Save</span>
+            </button>
+          </Form>
+        </Container>
+      </SignUpWrapper>
+    </>
+  );
+}
