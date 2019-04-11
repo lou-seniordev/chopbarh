ca94e2c96145dc3347301aa572dcad9b3abe0281
topskilldev <me@topskill.dev>
2019-04-11 01:30:52 +0100

    Adjusted Box shadows in Sign Up

--- src/components/Dashboard/UserProfile/EditProfileForm/EditProfileForm.js
+++ src/components/Dashboard/UserProfile/EditProfileForm/EditProfileForm.js
@@ -21,7 +21,7 @@ const Container = styled.div`
   width: 60vw;
   background: #fff;
   padding: 4rem 10rem;
-  box-shadow: 0px 18px 31px 13px rgba(214, 207, 214, 0.83);
+  box-shadow: 0px 18px 31px 0px rgba(214, 207, 214, 0.83);
 
   @media only screen and (max-width: ${breakPoints.mediumLite}) {
     padding: 4rem 5rem;
