b5a43207fcdc315aa4810854675014d3f4ab5332
topskilldev <me@topskill.dev>
2019-04-11 10:31:04 +0100

    Added View Logic to PayWithAccountNumber component

---
+++ src/components/Dashboard/UserDeposit/PayWithAccountNumber/PayWithAccountNumber.js
@@ -0,0 +1,115 @@
+import React from "react";
+import styled from "styled-components";
+import color from "../../../styles/colors";
+import breakPoints from "../../../styles/breakpoints";
+
+const Form = styled.form`
+  position: relative;
+
+  button {
+    all: unset;
+    padding: 0.5rem 1.3rem;
+    display: inline-block;
+    position: absolute;
+    left: 50%;
+    transform: skew(-20deg) translateX(-50%);
+    transition: all 0.2s;
+    color: ${color.colorWhite};
+    background: ${color.colorPrimary};
+    font-size: 1.3rem;
+    z-index: 200;
+
+    @media only screen and (max-width: ${breakPoints.mediumLite}) {
+      font-size: 1.1rem;
+    }
+
+    span {
+      display: inline-block;
+      transform: skew(20deg);
+      color: #fff;
+    }
+
+    &:hover {
+      transform: translateY(-3px) skew(-20deg) translateX(-50%);
+      background: ${color.colorPrimaryHover};
+      color: ${color.colorWhite};
+    }
+  }
+`;
+
+const FormItem = styled.div`
+  label {
+    font-size: 1.4rem;
+    font-weight: 600;
+    color: #737773;
+    margin-bottom: 1rem;
+  }
+
+  input,
+  select {
+    color: #8d8e8d;
+    width: 100%;
+    height: 3.4rem;
+    margin-bottom: 2rem;
+    border: 0;
+    background: #f6f6f6;
+    outline: none;
+    padding: 3px 5px;
+  }
+
+  & > * {
+    display: block;
+    font-family: inherit;
+  }
+`;
+
+const HalfColumn = styled.div`
+  display: flex;
+
+  @media only screen and (max-width: ${breakPoints.large}) {
+    flex-direction: column;
+  }
+
+  div {
+    width: 50%;
+
+    @media only screen and (max-width: ${breakPoints.large}) {
+      width: 100%;
+    }
+  }
+`;
+
+export default function PayWithAccountNumber() {
+  return (
+    <Form>
+      <FormItem>
+        <label>Choose Bank</label>
+        <select>
+          <option>Access Bank</option>
+          <option>First Bank</option>
+          <option>Fidelity Bank</option>
+          <option>Polaris Bank</option>
+        </select>
+      </FormItem>
+      <HalfColumn>
+        <FormItem className="mr-3">
+          <label>Account Name</label>
+          <input type="text" required />
+        </FormItem>
+        <FormItem>
+          <label>Account Number</label>
+          <input type="text" required />
+        </FormItem>
+      </HalfColumn>
+      <HalfColumn>
+        <FormItem className="mr-3">
+          <label>Amount</label>
+          <input type="number" required  />
+        </FormItem>
+      </HalfColumn>
+      <button type="submit" className="mr-2">
+        <span>Load</span>
+      </button>
+    </Form>
+  );
+}
