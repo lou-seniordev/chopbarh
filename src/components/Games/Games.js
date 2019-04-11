ee7d4836765bf9561f6f6431659899f1614c8eda
topskilldev <me@topskill.dev>
2019-04-11 01:19:14 +0100

    Added Page Title to Games Page

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import Header from "../UI/Header/Header";
 import Footer from "../UI/Footer/Footer";
 import GamesHero from "./GamesHero/GamesHero";
@@ -7,6 +8,9 @@ import GameItems from "./GameItems/GameItems";
 export default function Games() {
   return (
     <>
+      <Helmet>
+        <title>Chopbarh &rarr; Games</title>
+      </Helmet>
       <Header />
       <GamesHero />
       <GameItems />
