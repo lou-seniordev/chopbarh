7e3b545e985fd93a2dc84629e4c52583d2c1083d
topskilldev <me@topskill.dev>
2019-04-11 11:34:48 +0100

    Adjusted tabs style

--- src/components/Dashboard/UserWithdraw/WithdrawTabs/WithdrawTabs.js
+++ src/components/Dashboard/UserWithdraw/WithdrawTabs/WithdrawTabs.js
@@ -7,7 +7,7 @@ import Paga from "../Paga/Paga";
 const WithdrawTabsWrapper = styled.div`
   display: flex;
   justify-content: center;
-  padding: 5rem 2rem;
+  padding: 6rem 2rem;
 `;
 
 const HeadingTwo = styled.h2`
@@ -17,22 +17,24 @@ const HeadingTwo = styled.h2`
 export default function DepositTabs() {
   return (
     <WithdrawTabsWrapper className="container">
-      <Tabs>
-        <TabList>
-          <Tab>
-            <HeadingTwo>Phone Number</HeadingTwo>
-          </Tab>
-          <Tab>
-            <HeadingTwo>Paga</HeadingTwo>
-          </Tab>
-        </TabList>
-        <TabPanel>
-          <PhoneNumber />
-        </TabPanel>
-        <TabPanel>
-          <Paga />
-        </TabPanel>
-      </Tabs>
+      <div className="col-md-6">
+        <Tabs>
+          <TabList>
+            <Tab>
+              <HeadingTwo>Phone Number</HeadingTwo>
+            </Tab>
+            <Tab>
+              <HeadingTwo>Paga</HeadingTwo>
+            </Tab>
+          </TabList>
+          <TabPanel>
+            <PhoneNumber />
+          </TabPanel>
+          <TabPanel>
+            <Paga />
+          </TabPanel>
+        </Tabs>
+      </div>
     </WithdrawTabsWrapper>
   );
 }
