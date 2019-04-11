ec490afff8ddb596b6f3319e6350637a945ede26
topskilldev <me@topskill.dev>
2019-04-11 02:09:08 +0100

    Added UserDeposit base content

--- src/components/Dashboard/UserDeposit/Card/Card.js
+++ src/components/Dashboard/UserDeposit/Card/Card.js
@@ -7,7 +7,6 @@ export default () => (
       <Tab>Title 1</Tab>
       <Tab>Title 2</Tab>
     </TabList>
-
     <TabPanel>
       <h2>Any content 1</h2>
     </TabPanel>
--- src/components/Dashboard/UserDeposit/UserDeposit.js
+++ src/components/Dashboard/UserDeposit/UserDeposit.js
@@ -1,6 +1,19 @@
 import React from "react";
+import { Helmet } from "react-helmet";
+import UserHeader from "../shared/UserHeader/UserHeader";
+import Footer from "../../UI/Footer/Footer";
+import UserNavigation from "../shared/UserNavigation/UserNavigation";
 import Card from "./Card/Card";
 
 export default function UserDeposit() {
-  return <Card />;
+  return (
+    <>
+      <Helmet>
+        <title>Chopbarh &rarr; Dashboard</title>
+      </Helmet>
+      <UserHeader />
+      <UserNavigation />
+      <Footer />
+    </>
+  );
 }
