e92219b14729db5fe633f937351468d050e0fd5d
topskilldev <me@topskill.dev>
2019-04-11 02:18:36 +0100

    Added styling to DepositTabs component

--- src/components/Dashboard/UserDeposit/DepositTabs/DepositTabs.js
+++ src/components/Dashboard/UserDeposit/DepositTabs/DepositTabs.js
@@ -1,19 +1,41 @@
 import React from "react";
+import styled from "styled-components";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+
+const DepositTabsWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 5rem 2rem;
+`;
 
 export default function DepositTabs() {
   return (
-    <Tabs>
-      <TabList>
-        <Tab>Title 1</Tab>
-        <Tab>Title 2</Tab>
-      </TabList>
-      <TabPanel>
-        <h2>Any content 1</h2>
-      </TabPanel>
-      <TabPanel>
-        <h2>Any content 2</h2>
-      </TabPanel>
-    </Tabs>
+    <DepositTabsWrapper className="container">
+      <Tabs>
+        <TabList>
+          <Tab>
+            <h2>Card</h2>
+          </Tab>
+          <Tab>
+            <h2>Quickteller</h2>
+          </Tab>
+          <Tab>
+            <h2>Banks</h2>
+          </Tab>
+          <Tab>
+            <h2>Voucher</h2>
+          </Tab>
+          <Tab>
+            <h2>Pay with Account Number</h2>
+          </Tab>
+        </TabList>
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+    </DepositTabsWrapper>
   );
 }
