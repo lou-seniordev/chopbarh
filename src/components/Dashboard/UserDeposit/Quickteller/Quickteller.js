bc5759f17bf8f2b7fc51c4a0e9b345e443003982
topskilldev <me@topskill.dev>
2019-04-11 10:36:55 +0100

    Added base view logic to Quickteller component

---
+++ src/components/Dashboard/UserDeposit/Quickteller/Quickteller.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const QuicktellerWrapper = styled.div``;
+
+export default function Quickteller() {
+  return <QuicktellerWrapper>Quickteller Info</QuicktellerWrapper>;
+}
