ac85515883e2f822678b6e2529e77cc4e84525d7
topskilldev <me@topskill.dev>
2019-04-23 09:32:25 +0100

    Added ErrorBoundary to App component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from "react";
 import Layout from "./container/Layout";
+import ErrorBoundary from "./hoc/ErrorBoundary";
 
 export default function App() {
-  return <Layout />;
+  return (
+    <ErrorBoundary>
+      <Layout />
+    </ErrorBoundary>
+  );
 }
