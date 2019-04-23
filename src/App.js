0a965919560aec3322b7368a279e1b370623e67d
topskilldev <me@topskill.dev>
2019-04-23 09:38:00 +0100

    Added GlobalStyles to App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "./container/Layout";
 import ErrorBoundary from "./hoc/ErrorBoundary";
+import { GlobalStyles } from "./components/styles/GlobalStyles";
 
 export default function App() {
   return (
     <ErrorBoundary>
+      <GlobalStyles />
       <Layout />
     </ErrorBoundary>
   );
