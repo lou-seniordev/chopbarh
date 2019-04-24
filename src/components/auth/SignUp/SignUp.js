2e58bfc2e1f41e70916e9550276b4403cffc4c8e
topskilldev <me@topskill.dev>
2019-04-24 11:00:51 +0100

    Added formState to SignUp component

--- src/components/auth/SignUp/SignUp.js
+++ src/components/auth/SignUp/SignUp.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
+import { useFormState } from "react-use-form-state";
 import Header from "../../UI/Header/Header";
 import {
   SignUpWrapper,
@@ -13,65 +14,82 @@ import {
 } from "../../styles/SignUpStyles";
 
 export default function SignUp() {
-  
-    return (
-      <>
-        <Helmet>
-          <title>Chopbarh &rarr; Sign Up</title>
-        </Helmet>
-        <Header />
-        <SignUpWrapper>
-          <Container>
-            <Form>
-              <HeadingTwo className="mb-4">Sign Up</HeadingTwo>
-              <FormItem>
-                <label>Full Name</label>
-                <input type="text" required />
+  const [formState, { text, tel, date, email, password }] = useFormState();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    // API request here
+    console.log(formState);
+  };
+
+  return (
+    <>
+      <Helmet>
+        <title>Chopbarh &rarr; Sign Up</title>
+      </Helmet>
+      <Header />
+      <SignUpWrapper>
+        <Container>
+          <Form onSubmit={handleSubmit}>
+            <HeadingTwo className="mb-4">Sign Up</HeadingTwo>
+            <FormItem>
+              <label>Full Name</label>
+              <input {...text("fullname")} required />
+            </FormItem>
+            <FormItem>
+              <label>Phone Number</label>
+              <input {...tel("number")} required />
+            </FormItem>
+            <HalfColumn>
+              <FormItem className="mr-3">
+                <label>Date of Birth</label>
+                <input {...date("dob")} required />
               </FormItem>
               <FormItem>
-                <label>Phone Number</label>
-                <input type="tel" required />
+                <label>Sex</label>
+                <select>
+                  <option>Male</option>
+                  <option>Female</option>
+                  <option>Other</option>
+                </select>
               </FormItem>
-              <HalfColumn>
-                <FormItem className="mr-3">
-                  <label>Date of Birth</label>
-                  <input type="date" required />
-                </FormItem>
-                <FormItem>
-                  <label>Sex</label>
-                  <select>
-                    <option>Male</option>
-                    <option>Female</option>
-                    <option>Other</option>
-                  </select>
-                </FormItem>
-              </HalfColumn>
-              <HalfColumn>
-                <FormItem className="mr-3">
-                  <label>Enter Pin (4 to 6 digits)</label>
-                  <input type="password" required minLength="4" maxLength="6" />
-                </FormItem>
-                <FormItem>
-                  <label>Re-enter Pin</label>
-                  <input type="password" required minLength="4" maxLength="6" />
-                </FormItem>
-              </HalfColumn>
+            </HalfColumn>
+            <HalfColumn>
+              <FormItem className="mr-3">
+                <label>Enter Pin (4 to 6 digits)</label>
+                <input
+                  {...password("password")}
+                  required
+                  minLength="4"
+                  maxLength="6"
+                />
+              </FormItem>
               <FormItem>
-                <label>Email</label>
-                <input type="email" required />
+                <label>Re-enter Pin</label>
+                <input
+                  {...password("retype-password")}
+                  required
+                  minLength="4"
+                  maxLength="6"
+                />
               </FormItem>
-              <button type="submit" className="mr-2">
-                <span>Create Account</span>
-              </button>
-              <LoginSignal>
-                <p>By clicking, you agree to our Terms and Conditions</p>
-                <p>
-                  Already have an account? <Link to="login">Login</Link>
-                </p>
-              </LoginSignal>
-            </Form>
-          </Container>
-        </SignUpWrapper>
-      </>
-    );
-  }
+            </HalfColumn>
+            <FormItem>
+              <label>Email</label>
+              <input {...email("email")} required />
+            </FormItem>
+            <button type="submit" className="mr-2">
+              <span>Create Account</span>
+            </button>
+            <LoginSignal>
+              <p>By clicking, you agree to our Terms and Conditions</p>
+              <p>
+                Already have an account? <Link to="login">Login</Link>
+              </p>
+            </LoginSignal>
+          </Form>
+        </Container>
+      </SignUpWrapper>
+    </>
+  );
+}
