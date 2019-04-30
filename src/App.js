7a03cf28cdb261726f386359370c6c021674ca3b
topskilldev <me@topskill.dev>
2019-04-30 16:45:08 +0100

    Added ContextProvider to App component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import Layout from "./container/Layout";
 import ErrorBoundary from "./hoc/ErrorBoundary";
 import { GlobalStyles } from "./components/styles/GlobalStyles";
+import ContextProvider from "./hoc/ContextProvider";
 
 export default function App() {
   return (
     <ErrorBoundary>
-      <GlobalStyles />
-      <Layout />
+      <ContextProvider>
+        <GlobalStyles />
+        <Layout />
+      </ContextProvider>
     </ErrorBoundary>
   );
 }
