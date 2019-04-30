1699e09aafbdbe590eea408ca1e5b5697c3f2fe6
topskilldev <me@topskill.dev>
2019-04-30 14:36:57 +0100

    Added auth guard to UserDeposit component

--- src/components/Dashboard/UserDeposit/UserDeposit.js
+++ src/components/Dashboard/UserDeposit/UserDeposit.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import { Redirect } from "react-router-dom";
 import UserHeader from "../shared/UserHeader/UserHeader";
 import Footer from "../../UI/Footer/Footer";
 import UserNavigation from "../shared/UserNavigation/UserNavigation";
 import DepositTabs from "./DepositTabs/DepositTabs";
 
 export default function UserDeposit() {
+  const auth = localStorage.getItem("chopbarh-token");
+
   return (
     <>
+      {!auth && <Redirect to="/" />}
       <Helmet>
         <title>Chopbarh &rarr; Dashboard</title>
       </Helmet>
