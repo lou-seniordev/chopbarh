93020b3cc1cf25c9491685b21ebc9c196db33910
topskilldev <me@topskill.dev>
2019-04-30 17:20:09 +0100

    Removed component level auth guards

--- src/components/Dashboard/UserProfile/UserProfile.js
+++ src/components/Dashboard/UserProfile/UserProfile.js
@@ -1,16 +1,12 @@
 import React from "react";
-import { Redirect } from "react-router-dom";
 import UserHeader from "../shared/UserHeader/UserHeader";
 import UserNavigation from "../shared/UserNavigation/UserNavigation";
 import EditProfileForm from "./EditProfileForm/EditProfileForm";
 import Footer from "../../UI/Footer/Footer";
 
 export default function UserProfile() {
-  const auth = localStorage.getItem("chopbarh-token");
-
   return (
     <>
-      {!auth && <Redirect to="/" />}
       <UserHeader />
       <UserNavigation />
       <EditProfileForm />
