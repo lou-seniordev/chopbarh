6e631e90fbe83e6ffaca5b2d396205b001e2cc12
topskilldev <me@topskill.dev>
2019-05-01 15:49:15 +0100

    Removed unused imports from UserHeader component

--- src/components/Dashboard/shared/UserHeader/UserHeader.js
+++ src/components/Dashboard/shared/UserHeader/UserHeader.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
-import axios from "axios";
 import { Link } from "react-router-dom";
 import colors from "../../../styles/colors";
 import Logo from "../../../UI/Logo/Logo";
