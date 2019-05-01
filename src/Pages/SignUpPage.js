d4f0bafd7a65f98a7e28de3529d1e85d58f683b6
topskilldev <me@topskill.dev>
2019-05-01 11:03:05 +0100

    Removed unused logic from SignUpPage

--- src/Pages/SignUpPage.js
+++ src/Pages/SignUpPage.js
@@ -1,11 +1,6 @@
 import React from "react";
 import SignUp from "../components/auth/SignUp/SignUp";
-import { AppContext } from "../hoc/AppContext";
 
 export default function SignUpPage() {
-  return (
-    <AppContext.Consumer>
-      {({ getUserInfo }) => <SignUp getUserInfo={getUserInfo} />}
-    </AppContext.Consumer>
-  );
+  return <SignUp />;
 }
