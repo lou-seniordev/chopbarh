099a00a5bd4618eaeb7532a25427a018312eb4a6
topskilldev <me@topskill.dev>
2019-05-02 09:45:28 +0100

    Added Image content to UserProfileContent

--- src/components/Dashboard/UserProfile/UserProfileContent/UserProfileContent.js
+++ src/components/Dashboard/UserProfile/UserProfileContent/UserProfileContent.js
@@ -1,13 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import Ludo from "../../../assets/img/Ludo@2x.png";
+import Whot from "../../../assets/img/Whot@2x.png";
+
+const UserProfileContentWrapper = styled.div`
+  min-height: 30vh;
+`;
+
+const Image = styled.img`
+  border: 1px solid #ddd;
+  border-radius: 3px;
+  width: 100%;
+  height: 100%;
+  padding: 0.4rem;
+`;
 
 export default function UserProfileContent() {
   return (
-    <div className="container">
+    <UserProfileContentWrapper className="container my-5 pt-5 pb-5">
       <div className="row">
-        <div className="col-md-4">
-          <img src="..." alt="..." class="img-thumbnail" />
+        {/* <div className="col-md-4">
+          <Image src={Ludo} alt="Profile" class="img-thumbnail" />
         </div>
+        <div className="col-md-6">
+          <Image src={Whot} alt="Profile" class="img-thumbnail" />
+        </div> */}
       </div>
-    </div>
+    </UserProfileContentWrapper>
   );
 }
