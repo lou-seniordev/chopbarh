4bff3f5eabd2acd50431f5d7b5ed1491ef9dbcb8
topskilldev <me@topskill.dev>
2019-05-02 08:59:05 +0100

    Added User Edit Profile component

--- src/components/Dashboard/UserProfile/UserProfile.js
+++ src/components/Dashboard/UserProfile/UserProfile.js
@@ -1,7 +1,6 @@
 import React from "react";
 import UserHeader from "../shared/UserHeader/UserHeader";
 import UserNavigation from "../shared/UserNavigation/UserNavigation";
-import EditProfileForm from "./EditProfileForm/EditProfileForm";
 import Footer from "../../UI/Footer/Footer";
 
 export default function UserProfile() {
@@ -9,7 +8,6 @@ export default function UserProfile() {
     <>
       <UserHeader />
       <UserNavigation />
-      <EditProfileForm />
       <Footer />
     </>
   );
