b9d15801670ad4bdeb314baabe28cddf2dc3de90
topskilldev <me@topskill.dev>
2019-05-02 09:20:57 +0100

    Added base logic to UserProfileContent block

---
+++ src/components/Dashboard/UserProfile/UserProfileContent/UserProfileContent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function UserProfileContent() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-4">
+          <img src="..." alt="..." class="img-thumbnail" />
+        </div>
+      </div>
+    </div>
+  );
+}
