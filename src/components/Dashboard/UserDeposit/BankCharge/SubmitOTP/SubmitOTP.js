1f4a848415b76e3890667b306deb04392ec3f403
topskilldev <me@topskill.dev>
2019-05-06 12:34:20 +0100

    Added base logic to SubmitOTP component

---
+++ src/components/Dashboard/UserDeposit/BankCharge/SubmitOTP/SubmitOTP.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styled from "styled-components";
+import { useFormState } from "react-use-form-state";
+
+export default function SubmitOTP() {
+  return <div />;
+}
