1158965f3e4661ae92c230aa52552f662c06be5d
topskilldev <me@topskill.dev>
2019-05-08 11:10:02 +0100

    Added redux store to index

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./store/reducer";
+
+const store = createStore(reducer);
 
 const app = (
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById("root"));
