1601f1a649349e2ce391b7700d75b1d211f466af
topskilldev <me@topskill.dev>
2019-05-08 16:29:22 +0100

    Added connect for Redux to Layout component

--- src/container/Layout.js
+++ src/container/Layout.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import { AppContext } from "../hoc/AppContext";
+import { connect } from "react-redux";
 import LandingPage from "../Pages/LandingPage";
 import GamesPage from "../Pages/GamesPage";
 import LoginPage from "../Pages/LoginPage";
@@ -25,40 +25,47 @@ initialising in constructor and using componentWillMount
 
 */
 
-export default function Layout() {
-  return (
-    <AppContext.Consumer>
-      {({ authenticated }) => {
-        if (authenticated) {
-          return (
-            <Switch>
-              <Route path="/" exact component={LandingPage} />
-              <Route path="/games" component={GamesPage} />
-              <Route path="/reset" component={ForgotPassword} />
-              <Route path="/logout" component={Logout} />
-              <Route path="/user" component={UserHome} />
-              <Route path="/profile" component={ProfilePage} />
-              <Route path="/edit-profile" component={EditProfilePage} />
-              <Route path="/deposit" component={UserDeposit} />
-              <Route path="/withdraw" component={UserWithdraw} />
-              <Route path="/transaction" component={UserTransaction} />
-              <Redirect to="/" />
-            </Switch>
-          );
-        }
-        return (
-          <Switch>
-            <Route path="/" exact component={LandingPage} />
-            <Route path="/games" component={GamesPage} />
-            <Route path="/login" component={LoginPage} />
-            <Route path="/signup" component={SignUpPage} />
-            <Route path="/otp" component={OTP} />
-            <Route path="/set-nickname" component={SetNickname} />
-            <Route path="/complete_profile" component={CompleteProfilePage} />
-            <Redirect to="/" />
-          </Switch>
-        );
-      }}
-    </AppContext.Consumer>
-  );
+class Layout extends Component {
+  render() {
+    let routes = null;
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/games" component={GamesPage} />
+          <Route path="/reset" component={ForgotPassword} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/user" component={UserHome} />
+          <Route path="/profile" component={ProfilePage} />
+          <Route path="/edit-profile" component={EditProfilePage} />
+          <Route path="/deposit" component={UserDeposit} />
+          <Route path="/withdraw" component={UserWithdraw} />
+          <Route path="/transaction" component={UserTransaction} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    } else {
+      routes = (
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/games" component={GamesPage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/signup" component={SignUpPage} />
+          <Route path="/otp" component={OTP} />
+          <Route path="/set-nickname" component={SetNickname} />
+          <Route path="/complete_profile" component={CompleteProfilePage} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+
+    return <>{routes}</>;
+  }
 }
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.authenticated !== null
+});
+
+export default connect(mapStateToProps)(Layout);
