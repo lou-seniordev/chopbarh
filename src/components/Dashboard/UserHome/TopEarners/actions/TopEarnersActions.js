2128c50b0fc893725c12c563c01ba8aeb2b06a20
topskilldev <me@topskill.dev>
2019-05-09 01:13:50 +0100

    Corrected thunk function logic

--- src/components/Dashboard/UserHome/TopEarners/actions/TopEarnersActions.js
+++ src/components/Dashboard/UserHome/TopEarners/actions/TopEarnersActions.js
@@ -1,42 +1,44 @@
-import * as actionType from '../../../../../store/actionTypes/actionTypes'
+import * as actionType from "../../../../../store/actionTypes/actionTypes";
 
 export const fetchTopEarnersStart = () => ({
   type: actionType.FETCH_TOP_EARNERS_INIT
-})
+});
 
 export const fetchTopEarnersSuccess = () => ({
-type: actionType.FETCH_TOP_EARNERS_SUCCESS
-})
+  type: actionType.FETCH_TOP_EARNERS_SUCCESS
+});
 
 export const fetchTopEarnersFail = () => ({
-type: actionType.FETCH_TOP_EARNERS_FAIL
-})
+  type: actionType.FETCH_TOP_EARNERS_FAIL
+});
 
-export const fetchTopEarners = () => (dispatch, getState) => ({
+export const fetchTopEarners = () => (dispatch, getState) => {
   const postRequestData = {
-      "@class": ".LogEventRequest",
-      eventKey: "ANALYTICS_TOP_EARNERS",
-      playerId: getState().auth.id
-    };
+    "@class": ".LogEventRequest",
+    eventKey: "ANALYTICS_TOP_EARNERS",
+    playerId: getState().auth.id
+  };
 
-    fetch(
-      "https://c373328ysyuR.preview.gamesparks.net/rs/debug/AtfFvlREyWLhhmtWKbG13ASCyTCLLlm5/LogEventRequest",
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json"
-        },
-        data: JSON.stringify(postRequestData)
+  fetch(
+    "https://c373328ysyuR.preview.gamesparks.net/rs/debug/AtfFvlREyWLhhmtWKbG13ASCyTCLLlm5/LogEventRequest",
+    {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      data: JSON.stringify(postRequestData)
+    }
+  )
+    .then(response => response.json())
+    .then(data => {
+      if (data.error) {
+        dispatch(fetchTopEarnersFail());
+      } else {
+        dispatch(fetchTopEarnersSuccess());
       }
-    ).then(response => response.json())
-      .then(data => {
-        if (data.error) {
-          dispatch(fetchTopEarnersFail())
-        } else {
-          dispatch(fetchTopEarnersSuccess())
-        }
-      })
-      .catch(err => {dispatch(fetchTopEarnersFail())});
-})
-
+    })
+    .catch(err => {
+      dispatch(fetchTopEarnersFail());
+    });
+};
