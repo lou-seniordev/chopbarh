4e9344e6480424d0158c69e22083a654d911d639
topskilldev <me@topskill.dev>
2019-05-10 00:18:37 +0100

    Added modalActions logic

---
+++ src/components/Dashboard/UserDeposit/actions/modalActions.js
@@ -0,0 +1,17 @@
+import * as actionType from "../../../../store/actionTypes/actionTypes";
+
+export const openOTPModal = () => ({
+  type: actionType.OPEN_SUBMIT_OTP_MODAL
+});
+
+export const closeOTPModal = () => ({
+  type: actionType.CLOSE_SUBMIT_OTP_MODAL
+});
+
+export const openPinModal = () => ({
+  type: actionType.OPEN_SUBMIT_PIN_MODAL
+});
+
+export const closePinModal = () => ({
+  type: actionType.CLOSE_SUBMIT_OTP_MODAL
+});
