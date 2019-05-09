e97720cd95f1a6578192d93a765a0a935ede103d
topskilldev <me@topskill.dev>
2019-05-10 00:15:19 +0100

    Added logic  to modalReducer file

---
+++ src/store/reducers/modalReducer.js
@@ -0,0 +1,33 @@
+import * as actionType from "../actionTypes/actionTypes";
+
+const initialState = {
+  submitOTPModal: false,
+  submitPinModal: false
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case actionType.OPEN_SUBMIT_PIN_MODAL:
+      return {
+        ...state,
+        submitPinModal: true
+      };
+    case actionType.CLOSE_SUBMIT_PIN_MODAL:
+      return {
+        ...state,
+        submitPinModal: false
+      };
+    case actionType.OPEN_SUBMIT_OTP_MODAL:
+      return {
+        ...state,
+        submitOTPModal: false
+      };
+    case actionType.CLOSE_SUBMIT_OTP_MODAL:
+      return {
+        ...state,
+        submitOTPModal: false
+      };
+    default:
+      return state;
+  }
+};
