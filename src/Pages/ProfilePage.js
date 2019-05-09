7b5a942d97d933d2151b5f36332d65f00b84b9c8
topskilldev <me@topskill.dev>
2019-05-09 23:08:29 +0100

    Removed context from UserProfile

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,11 +1,6 @@
 import React from "react";
 import UserProfile from "../components/Dashboard/UserProfile/UserProfile";
-import { AppContext } from "../hoc/AppContext";
 
 export default function EditProfilePage() {
-  return (
-    <AppContext.Consumer>
-      {({ userGameData }) => <UserProfile userInfo={userGameData} />}
-    </AppContext.Consumer>
-  );
+  return <UserProfile />;
 }
