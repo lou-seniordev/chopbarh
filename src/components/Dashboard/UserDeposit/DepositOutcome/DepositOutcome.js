e2ef47b4785ffa99da1d6cc9745b0637bcf3a150
topskilldev <me@topskill.dev>
2019-05-09 17:44:53 +0100

    Added Header and Navigation to DepositOutcome component

--- src/components/Dashboard/UserDeposit/DepositOutcome/DepositOutcome.js
+++ src/components/Dashboard/UserDeposit/DepositOutcome/DepositOutcome.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
+import UserHeader from "../../shared/UserHeader/UserHeader";
+import Footer from "../../../UI/Footer/Footer";
+import UserNavigation from "../../shared/UserNavigation/UserNavigation";
 
 export default class DepositOutcome extends Component {
   render() {
-    return <div>Deposit Outcome</div>;
+    return (
+      <>
+        <UserHeader />
+        <UserNavigation />
+        <div style={{ minHeight: "36rem" }} />
+        <Footer />
+      </>
+    );
   }
 }
