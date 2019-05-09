ee2dc17c6a7798f144faa25cf4e2d6349f08a4bb
topskilldev <me@topskill.dev>
2019-05-09 23:29:56 +0100

    Added logic to SubmitPin component

---
+++ src/components/Dashboard/UserDeposit/BankCharge/SubmitPin/SubmitPin.js
@@ -0,0 +1,135 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router";
+import { Spinner } from "reactstrap";
+import { connect } from "react-redux";
+import { Form, FormItem } from "../../../../styles/CardCharge";
+
+class SubmitPin extends Component {
+  state = {
+    pin: "",
+    loading: false
+  };
+
+  formIsValid = ({ pin }) => {
+    if (!isNaN(pin) !== true || pin.length !== 4) {
+      return false;
+    }
+    return true;
+  };
+
+  handleInputChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  };
+
+  handleSubmit = async (event, closeModal) => {
+    event.preventDefault();
+    this.setState({ loading: true });
+
+    if (!this.formIsValid(this.state)) {
+      this.setState({ loading: false });
+      // Handle Error
+      return;
+    }
+
+    closeModal();
+
+    // const postData = {
+    //   otp: this.state.otp,
+    //   reference: this.props.reference
+    // };
+
+    // try {
+    //   const response = await fetch(
+    //     "https://api.paystack.co/charge/submit_pin",
+    //     {
+    //       method: "POST",
+    //       mode: "cors",
+    //       headers: {
+    //         Authorization: `Bearer sk_test_c644c86e3b42191b981bbc1c263f98c7020c9841`,
+    //         "Content-Type": "application/json"
+    //       },
+    //       body: JSON.stringify(postData)
+    //     }
+    //   );
+
+    //   const data = await response.json();
+    //   if (data.data) {
+    //     const value = +data.data.amount / 100;
+    //     this.props.setCoinBalance(value);
+    //     // this.props.history.push({
+    //     //   pathname: "/deposit/charge",
+    //     //   search: "?status=true"
+    //     // });
+    //   } else {
+    //     //Error here
+    //     // this.props.history.push({
+    //     //   pathname: "/deposit/charge",
+    //     //   search: "?status=false"
+    //     // });
+    //   }
+    //   console.log(data);
+    //   this.setState({ loading: false });
+    //   //const value = +data.data.amount / 100;
+    //   //   increaseCoinBalance(+data.data.amount / 100)
+    //   //     .then(response => response.json())
+    //   //     .then(data => {
+    //   //       console.log(data);
+    //   //       setCoinValue(value / 100);
+    //   //       setLoading(false);
+    //   //     })
+    //   //     .catch(err => {
+    //   //       console.log(err);
+    //   //       setLoading(false);
+    //   //     });
+    // } catch (err) {
+    //   console.log(err);
+    //   this.setState({ loading: false });
+    // }
+  };
+  render() {
+    return (
+      <Form onSubmit={ev => this.handleSubmit(ev, this.props.closeModal)}>
+        {this.state.loading ? (
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+            className="mt-5"
+          >
+            <Spinner />
+          </div>
+        ) : (
+          <>
+            <FormItem>
+              <label>Enter Pin</label>
+              <input
+                type="text"
+                name="otp"
+                value={this.state.pin}
+                onChange={this.handleInputChange}
+                required
+                placeholder="Pin"
+              />
+            </FormItem>
+            <button
+              type="submit"
+              className="mr-2"
+              disabled={this.state.loading}
+            >
+              <span>{this.state.loading ? "Processing..." : "Submit"}</span>
+            </button>
+          </>
+        )}
+      </Form>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  reference: state.charge.reference,
+  loading: state.coinBalance.loading
+});
+
+export default withRouter(connect(mapStateToProps)(SubmitPin));
