1e048dcc720ad35bd337c8ed48471e886369b5f0
topskilldev <me@topskill.dev>
2019-05-10 10:08:03 +0100

    Added new action creators for modal

--- src/components/Dashboard/UserDeposit/actions/modalActions.js
+++ src/components/Dashboard/UserDeposit/actions/modalActions.js
@@ -15,3 +15,19 @@ export const openOTPModal = () => ({
 export const closeOTPModal = () => ({
   type: actionType.CLOSE_SUBMIT_OTP_MODAL
 });
+
+export const openTransactionSuccessModal = () => ({
+  type: actionType.OPEN_TRANSACTION_SUCCESS_MODAL
+});
+
+export const closeTransactionSuccessModal = () => ({
+  type: actionType.CLOSE_TRANSACTION_SUCCESS_MODAL
+});
+
+export const openTransactionFailModal = () => ({
+  type: actionType.OPEN_TRANSACTION_FAIL_MODAL
+});
+
+export const closeTransactionFailModal = () => ({
+  type: actionType.CLOSE_TRANSACTION_FAIL_MODAL
+});
