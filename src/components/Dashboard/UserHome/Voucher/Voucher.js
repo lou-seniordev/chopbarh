47ea98bdd9457b35b2fbabfcfa0305b906eaf1a0
topskilldev <me@topskill.dev>
2019-05-13 13:42:09 +0100

    Added success modal to Voucher component

--- src/components/Dashboard/UserHome/Voucher/Voucher.js
+++ src/components/Dashboard/UserHome/Voucher/Voucher.js
@@ -66,11 +66,16 @@ class Voucher extends Component {
   state = {
     loading: false,
     voucherModal: false,
+    voucherSuccessModal: false,
     voucher: ""
   };
 
   voucherUsedModalToggle = () => {
     this.setState({ voucherModal: !this.state.voucherModal });
+  };
+
+  voucherSuccessModalToggle = () => {
+    this.setState({ voucherSuccessModal: !this.state.voucherSuccessModal });
   };
 
   handleInputChange = ({ target }) => {
@@ -94,24 +99,22 @@ class Voucher extends Component {
     }
 
     const postData = {
-      serial: this.state.voucher
+      pin: this.state.voucher,
+      reference: this.props.reference
     };
 
-    // formState.values["by"] = localStorage.getItem("chopbarh-id")
-    //   ? localStorage.getItem("chopbarh-id")
-    //   : null;
     const formValue = JSON.stringify(postData);
 
     try {
       const response = await fetch(
-        "https://cors-anywhere.herokuapp.com/https://partners.chopbarh.com/api/secured/vouchers/query",
+        "https://cors-anywhere.herokuapp.com/https://partners.chopbarh.com/api/voucher/use",
         {
           method: "POST",
           mode: "cors",
           headers: {
             "Content-Type": "application/json",
-            apikey: "C213-E3C9-C7",
-            Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImU1ODEwNjc2MzA1YjhlYzgwZjkxYzE2ZjBjYWE0NTU3MGYyYWQxMTA3OGFkMzdkZmZhYWM1YzE1OTA3MjA2NmUxMGUxZmQ0NTY2YzdhZWY1In0.eyJhdWQiOiIxIiwianRpIjoiZTU4MTA2NzYzMDViOGVjODBmOTFjMTZmMGNhYTQ1NTcwZjJhZDExMDc4YWQzN2RmZmFhYzVjMTU5MDcyMDY2ZTEwZTFmZDQ1NjZjN2FlZjUiLCJpYXQiOjE1NTUxMTczODksIm5iZiI6MTU1NTExNzM4OSwiZXhwIjoxNTg2NzM5Nzg5LCJzdWIiOiIwNzVkOTM4MC05MTdmLTExZTgtYmQ4Yy1kNzU0YTJlOTlhMjMiLCJzY29wZXMiOltdfQ.SjLzlUTJZ5Aa9T7XhVkWDRLqk87iO1AvNdmA1_G7KODpQmaOoIsAXLB9DmuYijxfOejFIa89kwzHOM5S5IUNUOddyDtUR0Cb8l94igllauLHINOyihJ4rW04xEdGLpobyssSXUiwEjtu1q6HCHOrdQx-3AM6h9zYBPH047nTjyUUmq3d3eFIKC_s7A1rYr5hHCgJQk8-AYJrca8v_qhvTyB_XX48Van2K1-e8BSIskC_5R14946S7x1B83dKC96zac8tHCqtt6rdhF32JyJgnn-OQe9y2QjqGPXF0i3oLvAr1Y2AWTkul3wqkOQ4gmiD77TOFDlnXslCuNw1nYgNJlfLwuvVa9UuG-3bODC8zKhhPjDkJkEEiz3umGYJ7MluDP2WfAwYjkaEFpfTVy6lNJT91h0AHpRiNUZzW5FBKboXDStm8r7n9caOsQ2vyOfPZOZ53Hyc4skAL5wX47-Yr6zN3Keb9d3MnpHKZ9EbXpoFyMys4FBy5BMtER3fFH4GA2RROevpBYdxRenwMyVl39aQKVRaVQHxVPoYA7voMQuVY1iI-nykJpjhoJNsNsNiKE2P0x3bLxQxCCVuSujzJ-zByd8HP-GIwzn-zh1BvFWA9-5EF7qcnso7ttrXgrF4FuSwCH1y4YSaWu3l0LgyJ3K4pCtKl6FKiU0ttrsLNIE `
+            apikey: "C213-E3C9-C7"
+            //Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImU1ODEwNjc2MzA1YjhlYzgwZjkxYzE2ZjBjYWE0NTU3MGYyYWQxMTA3OGFkMzdkZmZhYWM1YzE1OTA3MjA2NmUxMGUxZmQ0NTY2YzdhZWY1In0.eyJhdWQiOiIxIiwianRpIjoiZTU4MTA2NzYzMDViOGVjODBmOTFjMTZmMGNhYTQ1NTcwZjJhZDExMDc4YWQzN2RmZmFhYzVjMTU5MDcyMDY2ZTEwZTFmZDQ1NjZjN2FlZjUiLCJpYXQiOjE1NTUxMTczODksIm5iZiI6MTU1NTExNzM4OSwiZXhwIjoxNTg2NzM5Nzg5LCJzdWIiOiIwNzVkOTM4MC05MTdmLTExZTgtYmQ4Yy1kNzU0YTJlOTlhMjMiLCJzY29wZXMiOltdfQ.SjLzlUTJZ5Aa9T7XhVkWDRLqk87iO1AvNdmA1_G7KODpQmaOoIsAXLB9DmuYijxfOejFIa89kwzHOM5S5IUNUOddyDtUR0Cb8l94igllauLHINOyihJ4rW04xEdGLpobyssSXUiwEjtu1q6HCHOrdQx-3AM6h9zYBPH047nTjyUUmq3d3eFIKC_s7A1rYr5hHCgJQk8-AYJrca8v_qhvTyB_XX48Van2K1-e8BSIskC_5R14946S7x1B83dKC96zac8tHCqtt6rdhF32JyJgnn-OQe9y2QjqGPXF0i3oLvAr1Y2AWTkul3wqkOQ4gmiD77TOFDlnXslCuNw1nYgNJlfLwuvVa9UuG-3bODC8zKhhPjDkJkEEiz3umGYJ7MluDP2WfAwYjkaEFpfTVy6lNJT91h0AHpRiNUZzW5FBKboXDStm8r7n9caOsQ2vyOfPZOZ53Hyc4skAL5wX47-Yr6zN3Keb9d3MnpHKZ9EbXpoFyMys4FBy5BMtER3fFH4GA2RROevpBYdxRenwMyVl39aQKVRaVQHxVPoYA7voMQuVY1iI-nykJpjhoJNsNsNiKE2P0x3bLxQxCCVuSujzJ-zByd8HP-GIwzn-zh1BvFWA9-5EF7qcnso7ttrXgrF4FuSwCH1y4YSaWu3l0LgyJ3K4pCtKl6FKiU0ttrsLNIE `
           },
           body: formValue
         }
@@ -123,8 +126,9 @@ class Voucher extends Component {
         this.setState({ loading: false });
         // Open the modal here
         // const value = data.data.value;
-      } else if (response.status === 422) {
+      } else if (response.status === 404) {
         // setVoucherUsedModal(true);
+        this.setState({ voucherModal: true });
         this.setState({ loading: false });
       } else {
         this.setState({ loading: false });
@@ -145,6 +149,21 @@ class Voucher extends Component {
         >
           <ModalBody className="text-center">
             <p>This Voucher has already been used</p>
+          </ModalBody>
+        </Modal>
+        <Modal
+          isOpen={this.state.voucherSuccessModal}
+          toggle={this.voucherSuccessModalToggle}
+          style={{
+            marginTop: "22rem"
+          }}
+        >
+          <ModalBody
+            className="text-center"
+            style={{ height: "20vh", paddingTop: "4rem" }}
+          >
+            <h2>Success!</h2>
+            <p>The Voucher was successfully loaded</p>
           </ModalBody>
         </Modal>
         <FormWrapper onSubmit={this.handleSubmit}>
@@ -175,11 +194,15 @@ class Voucher extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  reference: state.auth.id
+});
+
 const mapDispatchToProps = {
   setVoucherValue
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Voucher);
