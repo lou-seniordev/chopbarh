7b2ffa2dba0625732dabf005c3c123feb445f76a
topskilldev <me@topskill.dev>
2019-05-14 11:35:57 +0100

    Added new logic to Paga Withdrawal

--- src/components/Dashboard/UserWithdraw/Paga/Paga.js
+++ src/components/Dashboard/UserWithdraw/Paga/Paga.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import styled from "styled-components";
+import { Modal, ModalBody } from "reactstrap";
+import CryptoJS from "crypto-js";
 import color from "../../../styles/colors";
 import breakPoints from "../../../styles/breakpoints";
+import {
+  openTransactionSuccessModal,
+  openTransactionFailModal,
+  closeTransactionFailModal,
+  closeTransactionSuccessModal
+} from "../../UserDeposit/actions/modalActions";
+import { setCashBalance } from "../../shared/actions/cashBalanceActions";
 
 const Form = styled.form`
   position: relative;
@@ -79,22 +89,237 @@ const HalfColumn = styled.div`
   }
 `;
 
-export default function Paga() {
-  return (
-    <Form>
-      <HalfColumn>
-        <FormItem className="mr-3">
-          <label>Amount</label>
-          <input type="number" required />
-        </FormItem>
-        <FormItem>
-          <label>Phone Number</label>
-          <input type="number" required />
-        </FormItem>
-      </HalfColumn>
-      <button type="submit" className="mr-2">
-        <span>Withdraw</span>
-      </button>
-    </Form>
-  );
+class Paga extends Component {
+  state = {
+    loading: false,
+    formErrorModal: false,
+    overdraftModal: false,
+    successModal: false,
+    amount: "",
+    phone: ""
+  };
+
+  formErrorModalToggle = () => {
+    this.setState({ formErrorModal: !this.state.formErrorModal });
+  };
+
+  successModalToggle = () => {
+    this.setState({ successModal: !this.state.successModal });
+  };
+
+  overdraftModalToggle = () => {
+    this.setState({ overdraftModal: !this.state.overdraftModal });
+  };
+
+  handleInputChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  };
+
+  formIsValid = ({ amount, phone }) => {
+    if (
+      !isNaN(amount) !== true ||
+      !isNaN(phone) !== true ||
+      phone.length < 10
+    ) {
+      return false;
+    }
+    return true;
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    this.setState({ loading: true });
+
+    if (!this.formIsValid(this.state)) {
+      this.setState({ formErrorModal: true });
+      this.setState({ loading: false });
+      return;
+    }
+
+    // if (this.state.amount > this.props.playerData.RealCoins) {
+    //   this.setState({ overdraftModal: true });
+    //   return;
+    // }
+
+    var hashParameter = "referenceNumber,amount,destinationAccount";
+
+    var params = ("" + hashParameter).split(",");
+    var body = {
+      referenceNumber: "refe_135676",
+      amount: this.state.amount,
+      minRecipientKYCLevel: "KYC2",
+      destinationAccount: "2348063645616",
+      alternateSenderName: "BitPesa"
+    };
+
+    hashParameter = "";
+    for (var i in params) {
+      console.log(body[params[i]]);
+      hashParameter += body[params[i]] || "";
+    }
+    hashParameter =
+      hashParameter +
+      "d98076e2d14c4045970edc466faa2ec8cc47c9b89b654001b5e4db27179a0b9559bee92b78034c558a9d24aca2fa4135db8938a3f4a74b7da1157dee68e15213";
+    let hash = CryptoJS.SHA512(hashParameter);
+    hash = hash.toString(CryptoJS.enc.Hex);
+    console.log(hash);
+
+    // fetch(
+    //   "https://qa1.mypaga.com/paga-webservices/business-rest/secured/moneyTransfer",
+    //   {
+    //     method: "POST",
+    //     mode: "cors",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Principal: "98F32858-CC3B-42D4-95A3-742110A8D405",
+    //       Credentials: "rR9@f8u@bBES",
+    //       Hash: hash
+    //     },
+    //     body: JSON.stringify(body)
+    //   }
+    // )
+    //   .then(response => response.json())
+    //   .then(data => console.log(data))
+    //   .catch(err => console.log('Error', err));
+
+    // try {
+    //   const response = await fetch(
+    //     "https://ravesandboxapi.flutterwave.com/v2/gpx/transfers/create",
+    //     {
+    //       method: "POST",
+    //       mode: "cors",
+    //       headers: {
+    //         "Content-Type": "application/json"
+    //       },
+    //       body: JSON.stringify(postData)
+    //     }
+    //   );
+    //   const data = await response.json();
+    //   this.setState({
+    //     loading: false,
+    //     amount: "",
+    //     bank: "",
+    //     account_number: ""
+    //   });
+    //   if (response.status === 200) {
+    //     this.props.openTransactionSuccessModal();
+    //     // this.props.setCashBalance(data.data.amount, 2);
+    //   } else {
+    //     this.props.openTransactionFailModal();
+    //   }
+    // } catch (err) {
+    //   this.setState({ loading: false });
+    //   this.setState({ formErrorModal: true });
+    // }
+  };
+
+  render() {
+    return (
+      <>
+        <Modal
+          isOpen={this.state.formErrorModal}
+          toggle={this.formErrorModalToggle}
+          style={{
+            marginTop: "22rem"
+          }}
+        >
+          <ModalBody className="text-center" style={{ height: "20vh" }}>
+            <h2>Ooops!</h2>
+            <p>Something went wrong. Please try again</p>
+          </ModalBody>
+        </Modal>
+        <Modal
+          isOpen={this.props.transactionSuccessModal}
+          toggle={this.props.closeTransactionSuccessModal}
+          style={{
+            marginTop: "22rem"
+          }}
+        >
+          <ModalBody
+            className="text-center"
+            style={{ height: "20vh", paddingTop: "4rem" }}
+          >
+            <h2>Success!</h2>
+            <p>The Transaction was successful</p>
+          </ModalBody>
+        </Modal>
+        <Modal
+          isOpen={this.props.transactionFailModal}
+          toggle={this.props.closeTransactionFailModal}
+          style={{
+            marginTop: "22rem"
+          }}
+        >
+          <ModalBody
+            className="text-center"
+            style={{ height: "20vh", paddingTop: "4rem" }}
+          >
+            <h2>Failed!</h2>
+            <p>The transaction was not successful. Please try again</p>
+          </ModalBody>
+        </Modal>
+        <Modal
+          isOpen={this.state.overdraftModal}
+          toggle={this.overdraftModalToggle}
+          style={{
+            marginTop: "22rem"
+          }}
+        >
+          <ModalBody
+            className="text-center"
+            style={{ height: "20vh", paddingTop: "4rem" }}
+          >
+            <h2>Oh Snap!</h2>
+            <p>The amount is above your winnings</p>
+          </ModalBody>
+        </Modal>
+        <Form>
+          <HalfColumn>
+            <FormItem className="mr-3">
+              <label>Amount</label>
+              <input
+                type="number"
+                name="amount"
+                value={this.state.amount}
+                onChange={this.handleInputChange}
+                required
+              />
+            </FormItem>
+            <FormItem>
+              <label>Phone Number</label>
+              <input
+                type="number"
+                name="phone"
+                value={this.state.phone}
+                onChange={this.handleInputChange}
+                required
+              />
+            </FormItem>
+          </HalfColumn>
+          <button type="submit" className="mr-2">
+            <span>Withdraw</span>
+          </button>
+        </Form>
+      </>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  transactionSuccessModal: state.modal.transactionSuccessModal,
+  transactionFailModal: state.modal.transactionFailModal,
+  playerData: state.player.playerData
+});
+
+const mapDispatchToProps = {
+  openTransactionSuccessModal,
+  openTransactionFailModal,
+  closeTransactionFailModal,
+  closeTransactionSuccessModal,
+  setCashBalance
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Paga);
