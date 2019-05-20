35f932512f68cc10f47ef16c09c6f154e650e8f9
topskilldev <me@topskill.dev>
2019-05-21 00:38:45 +0100

    Changed redux devtools production logic

--- src/index.js
+++ src/index.js
@@ -25,12 +25,10 @@ const rootReducer = combineReducers({
   voucher: voucherReducer
 });
 
-// const composeEnhancers =
-//   process.env.NODE_ENV === "development"
-//     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-//     : null || compose;
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+  process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null || compose;
 
 const store = createStore(
   rootReducer,
