61c0a40943badc21592ed38040499a418f3523be
topskilldev <me@topskill.dev>
2019-05-20 13:25:59 +0100

    Added new action to LoginActions

--- src/components/auth/Login/actions/LoginActions.js
+++ src/components/auth/Login/actions/LoginActions.js
@@ -4,6 +4,15 @@ export const authStart = () => ({
   type: actionType.AUTH_START
 });
 
+export const authOTPGenerator = () => {
+  const otp = Math.floor(111111 + Math.random() * 999999);
+
+  return {
+    type: actionType.AUTH_OTP,
+    otp
+  };
+};
+
 export const authSuccess = (token, id) => ({
   type: actionType.AUTH_SUCCESS,
   token,
