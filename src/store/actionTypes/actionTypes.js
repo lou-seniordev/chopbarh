ad60b89433b92e33e043b45eaf5440ba7bbb872a
topskilldev <me@topskill.dev>
2019-05-20 13:22:45 +0100

    Added new actionType

--- src/store/actionTypes/actionTypes.js
+++ src/store/actionTypes/actionTypes.js
@@ -1,4 +1,5 @@
 export const AUTH_START = "AUTH_START";
+export const AUTH_OTP = "AUTH_OTP";
 export const AUTH_SUCCESS = "AUTH_SUCCESS";
 export const AUTH_FAILURE = "AUTH_FAILURE";
 export const AUTH_LOGOUT = "AUTH_LOGOUT";
