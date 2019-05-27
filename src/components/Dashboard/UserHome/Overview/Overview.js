f3b2b8ecc1e37a3eb5ea18ee516993345564b1aa
topskilldev <me@topskill.dev>
2019-05-27 02:21:26 +0100

    Added memo to Overview component

--- src/components/Dashboard/UserHome/Overview/Overview.js
+++ src/components/Dashboard/UserHome/Overview/Overview.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, memo } from "react";
 import styled from "styled-components";
 import { connect } from "react-redux";
 import { Spinner } from "reactstrap";
@@ -100,4 +100,4 @@ const mapDispatchToProps = {};
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(Overview);
+)(memo(Overview));
