dbeb44f2dab6487274d103f51e5eedd5f90e42da
topskilldev <me@topskill.dev>
2019-05-27 01:36:23 +0100

    Added logic to UserTransactionPage

---
+++ src/Pages/UserTransactionPage.js
@@ -0,0 +1,6 @@
+import React from "react";
+import UserTransaction from "../components/Dashboard/UserTransaction/UserTransaction";
+
+export default function UserTransactionPage() {
+  return <UserTransaction />;
+}
