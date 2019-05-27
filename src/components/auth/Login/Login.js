f6c27df85b68d9b0a1eb66eb4dadc9b6c8d4fd6d
topskilldev <me@topskill.dev>
2019-05-27 01:53:35 +0100

    Added memo to Login component

--- src/components/auth/Login/Login.js
+++ src/components/auth/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, memo } from "react";
 import { Helmet } from "react-helmet";
 import { withRouter, Link } from "react-router-dom";
 //import axios from "axios";
@@ -194,5 +194,5 @@ export default withRouter(
   connect(
     mapStateToProps,
     mapDispatchToProps
-  )(Login)
+  )(memo(Login))
 );
