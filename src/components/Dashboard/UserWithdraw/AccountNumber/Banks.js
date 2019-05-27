99ce8ec59f95a78813e59be5768ca8c2119ac511
topskilldev <me@topskill.dev>
2019-05-27 18:22:44 +0100

    Added values to Banks file

---
+++ src/components/Dashboard/UserWithdraw/AccountNumber/Banks.js
@@ -0,0 +1,107 @@
+export default [
+  {
+    Id: 137,
+    Code: "011",
+    Name: "First Bank of Nigeria"
+  },
+  {
+    Id: 141,
+    Code: "057",
+    Name: "Zenith Bank"
+  },
+  {
+    Id: 142,
+    Code: "068",
+    Name: "Standard Chartered Bank"
+  },
+  {
+    Id: 144,
+    Code: "070",
+    Name: "Fidelity Bank"
+  },
+  {
+    Id: 145,
+    Code: "023",
+    Name: "CitiBank"
+  },
+  {
+    Id: 146,
+    Code: "215",
+    Name: "Unity Bank"
+  },
+  {
+    Id: 151,
+    Code: "301",
+    Name: "JAIZ Bank"
+  },
+  {
+    Id: 152,
+    Code: "050",
+    Name: "Ecobank Plc"
+  },
+  {
+    Id: 158,
+    Code: "221",
+    Name: "Stanbic IBTC Bank"
+  },
+  {
+    Id: 159,
+    Code: "501",
+    Name: "Fortis Microfinance Bank"
+  },
+  {
+    Id: 168,
+    Code: "035",
+    Name: "Wema Bank"
+  },
+  {
+    Id: 170,
+    Code: "063",
+    Name: "Diamond Bank"
+  },
+  {
+    Id: 172,
+    Code: "100",
+    Name: "SunTrust Bank"
+  },
+  {
+    Id: 177,
+    Code: "058",
+    Name: "GTBank Plc"
+  },
+  {
+    Id: 178,
+    Code: "032",
+    Name: "Union Bank"
+  },
+  {
+    Id: 179,
+    Code: "232",
+    Name: "Sterling Bank"
+  },
+  {
+    Id: 180,
+    Code: "076",
+    Name: "Skye Bank"
+  },
+  {
+    Id: 181,
+    Code: "082",
+    Name: "Keystone Bank"
+  },
+  {
+    Id: 186,
+    Code: "214",
+    Name: "First City Monument Bank"
+  },
+  {
+    Id: 190,
+    Code: "033",
+    Name: "United Bank for Africa"
+  },
+  {
+    Id: 191,
+    Code: "044",
+    Name: "Access Bank"
+  }
+];
