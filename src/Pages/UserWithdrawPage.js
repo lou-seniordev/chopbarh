239eb72f33949e36ddb6f335ec5642730cf7eb93
topskilldev <me@topskill.dev>
2019-05-27 01:37:18 +0100

    Added logic to UserWithdrawPage

---
+++ src/Pages/UserWithdrawPage.js
@@ -0,0 +1,6 @@
+import React from "react";
+import UserWithdraw from "../components/Dashboard/UserWithdraw/UserWithdraw";
+
+export default function UserWithdrawPage() {
+  return <UserWithdraw />;
+}
