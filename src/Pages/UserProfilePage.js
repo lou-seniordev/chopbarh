b5f8b27943aba4b1ea0613a733f2b795d1657706
topskilldev <me@topskill.dev>
2019-05-27 09:35:17 +0100

    Changed default export in UserProfilePage

--- src/Pages/UserProfilePage.js
+++ src/Pages/UserProfilePage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import UserProfile from "../components/Dashboard/UserProfile/UserProfile";
 
-export default function EditProfilePage() {
+export default function UserProfilePage() {
   return <UserProfile />;
 }
