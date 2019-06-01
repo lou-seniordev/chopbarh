c68382ddf65bbc52de7a1a631f26ab2f0783ce91
topskilldev <me@topskill.dev>
2019-06-01 14:53:06 +0100

    Added new action creators to creditCardActions

---
+++ src/store/actions/creditCardActions.js
@@ -0,0 +1,46 @@
+import * as actionType from "../actionTypes/actionTypes";
+import firebase, { firestore } from "../../firebase";
+
+export const fetchCreditCardInit = () => ({
+  type: actionType.FETCH_CREDIT_CARD_INIT
+});
+
+export const fetchCreditCardSuccess = data => ({
+  type: actionType.FETCH_CREDIT_CARD_SUCCESS,
+  data
+});
+
+export const fetchCreditCardFail = () => ({
+  type: actionType.FETCH_CREDIT_CARD_FAIL
+});
+
+export const fetchCreditCardData = () => async (dispatch, getState) => {
+  dispatch(fetchCreditCardInit());
+
+  try {
+    const snapshot = await firestore
+      .collection("card_charge")
+      .where("id", "==", getState().auth.id)
+      .get();
+
+    const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    console.log(data);
+    dispatch(fetchCreditCardSuccess(data[0].data));
+  } catch (err) {
+    console.log("Error...", err);
+    dispatch(fetchCreditCardFail());
+  }
+};
+
+export const setCreditCardInit = () => ({
+  type: actionType.SET_CREDIT_CARD_INIT
+});
+
+export const setCreditCardSuccess = data => ({
+  type: actionType.SET_CREDIT_CARD_SUCCESS,
+  data
+});
+
+export const setCreditCardFail = () => ({
+  type: actionType.SET_CREDIT_CARD_FAIL
+});
