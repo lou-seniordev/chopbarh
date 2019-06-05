481c462972050557287a76321c067f9ca328580e
topskilldev <me@topskill.dev>
2019-06-05 09:47:43 +0100

    Added new clicked prop to Icon component

--- src/components/Dashboard/shared/Icon/Icon.js
+++ src/components/Dashboard/shared/Icon/Icon.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function Icon({ icon, height }) {
-  return <img src={icon} className="mx-3" alt="Icon" height={height} />;
+export default function Icon({ icon, height, clicked }) {
+  return (
+    <img
+      onClick={clicked}
+      src={icon}
+      className="mx-3"
+      alt="Icon"
+      height={height}
+    />
+  );
 }
