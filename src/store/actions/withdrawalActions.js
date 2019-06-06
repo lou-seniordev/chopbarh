6cae3237567470d52440207f9eaee52a80859458
topskilldev <me@topskill.dev>
2019-06-06 01:31:16 +0100

    Added action creators to withdrawal actions

---
+++ src/store/actions/withdrawalActions.js
@@ -0,0 +1,96 @@
+import * as actionType from "../actionTypes/actionTypes";
+import firebase, { firestore } from "../../firebase";
+
+export const fetchWithdrawalHistoryInit = () => ({
+  type: actionType.FETCH_WITHDRAWAL_HISTORY_INIT
+});
+
+export const fetchWithdrawalHistorySuccess = data => ({
+  type: actionType.FETCH_WITHDRAWAL_HISTORY_SUCCESS,
+  data
+});
+
+export const fetchWithdrawalHistoryFail = () => ({
+  type: actionType.FETCH_WITHDRAWAL_HISTORY_FAIL
+});
+
+export const fetchWithdrawalHistoryData = () => async (dispatch, getState) => {
+  dispatch(fetchWithdrawalHistoryInit());
+
+  try {
+    const snapshot = await firestore
+      .collection("withdrawals")
+      .where("id", "==", getState().auth.id)
+      .get();
+
+    const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    if (data.length) {
+      dispatch(fetchWithdrawalHistorySuccess(data[0].data));
+    } else {
+      dispatch(fetchWithdrawalHistoryFail());
+    }
+  } catch (err) {
+    dispatch(fetchWithdrawalHistoryFail());
+  }
+};
+
+export const setWithdrawalHistoryInit = () => ({
+  type: actionType.SET_WITHDRAWAL_HISTORY_INIT
+});
+
+export const setWithdrawalHistorySuccess = data => ({
+  type: actionType.SET_WITHDRAWAL_HISTORY_SUCCESS,
+  data
+});
+
+export const setWithdrawalHistoryFail = () => ({
+  type: actionType.SET_WITHDRAWAL_HISTORY_FAIL
+});
+
+export const setWithdrawalHistory = payload => async (dispatch, getState) => {
+  dispatch(setWithdrawalHistoryInit());
+  try {
+    const snapshot = await firestore
+      .collection("withdrawals")
+      .where("id", "==", getState().auth.id)
+      .get();
+
+    const withdrawals = snapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+
+    if (withdrawals.length) {
+      const docRef = await firestore
+        .collection("withdrawals")
+        .doc(getState().auth.id)
+        .update({
+          data: firebase.firestore.FieldValue.arrayUnion({
+            amount: payload.amount,
+            channel: payload.channel,
+            withdrawal_date: payload.withdrawal_date,
+            paid_at: payload.paid_at
+          })
+        });
+      dispatch(setWithdrawalHistorySuccess(docRef));
+    } else {
+      const docRef = await firestore
+        .collection("withdrawals")
+        .doc(getState().auth.id)
+        .set({
+          id: getState().auth.id,
+          data: [
+            {
+              amount: payload.amount,
+              channel: payload.channel,
+              withdrawal_date: payload.withdrawal_date,
+              paid_at: payload.paid_at
+            }
+          ]
+        });
+      dispatch(setWithdrawalHistorySuccess(docRef));
+    }
+  } catch (err) {
+    dispatch(setWithdrawalHistoryFail());
+  }
+};
