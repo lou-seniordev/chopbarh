04fa1a43c1936cd584d322f8457b4279cd535f97
topskilldev <me@topskill.dev>
2019-06-06 01:22:00 +0100

    Added base logic to withdrawalReducer

---
+++ src/store/reducers/withdrawalReducer.js
@@ -0,0 +1,37 @@
+import * as actionType from "../actionTypes/actionTypes";
+
+const initialState = {
+  loading: true,
+  withdrawalHistory: null,
+  error: false
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case actionType.FETCH_WITHDRAWAL_HISTORY_INIT:
+      return {
+        ...state,
+        loading: true
+      };
+    case actionType.FETCH_WITHDRAWAL_HISTORY_SUCCESS:
+      return {
+        ...state,
+        withdrawalHistory: [...action.data],
+        loading: false
+      };
+    case actionType.FETCH_WITHDRAWAL_HISTORY_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: true
+      };
+    case actionType.AUTH_LOGOUT:
+      return {
+        ...state,
+        loading: true,
+        transactionHistory: null
+      };
+    default:
+      return state;
+  }
+};
