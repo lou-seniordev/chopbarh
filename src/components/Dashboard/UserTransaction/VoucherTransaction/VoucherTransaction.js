7e583498a4519e21de032be2969be26773f19755
topskilldev <me@topskill.dev>
2019-06-06 16:45:04 +0100

    Removed unused styles from UserTransaction component

--- src/components/Dashboard/UserTransaction/VoucherTransaction/VoucherTransaction.js
+++ src/components/Dashboard/UserTransaction/VoucherTransaction/VoucherTransaction.js
@@ -22,66 +22,66 @@ const VoucherTransactionWrapper = styled.div`
   }
 `;
 
-const FormWrapper = styled.div`
-  display: flex;
-  align-items: center;
+// const FormWrapper = styled.div`
+//   display: flex;
+//   align-items: center;
 
-  button {
-    all: unset;
-    padding: 0.5rem 1.3rem;
-    transform: skew(-20deg);
-    display: inline-block;
-    transition: all 0.2s;
-    color: ${color.colorWhite};
-    background: ${color.colorPrimary};
-    font-size: 1.3rem;
-    z-index: 200;
+//   button {
+//     all: unset;
+//     padding: 0.5rem 1.3rem;
+//     transform: skew(-20deg);
+//     display: inline-block;
+//     transition: all 0.2s;
+//     color: ${color.colorWhite};
+//     background: ${color.colorPrimary};
+//     font-size: 1.3rem;
+//     z-index: 200;
 
-    /* @media only screen and (max-width: ${breakPoints.mediumLite}) {
-      align-self: flex-start;
-      padding: 0.5rem 1.7rem;
-      margin-bottom: 0.5rem;
-      margin-top: 0.5rem;
-    } */
+//     /* @media only screen and (max-width: ${breakPoints.mediumLite}) {
+//       align-self: flex-start;
+//       padding: 0.5rem 1.7rem;
+//       margin-bottom: 0.5rem;
+//       margin-top: 0.5rem;
+//     } */
 
-    span {
-      display: inline-block;
-      transform: skew(20deg);
-      color: #fff;
-    }
+//     span {
+//       display: inline-block;
+//       transform: skew(20deg);
+//       color: #fff;
+//     }
 
-    &:hover {
-      transform: translateY(-3px) skew(-20deg);
-      background: ${color.colorPrimaryHover};
-      color: ${color.colorWhite};
-    }
-  }
-`;
+//     &:hover {
+//       transform: translateY(-3px) skew(-20deg);
+//       background: ${color.colorPrimaryHover};
+//       color: ${color.colorWhite};
+//     }
+//   }
+// `;
 
-const FormItem = styled.div`
-  label {
-    font-size: 1.4rem;
-    font-weight: 600;
-    color: #737773;
-    margin-bottom: 1rem;
-  }
+// const FormItem = styled.div`
+//   label {
+//     font-size: 1.4rem;
+//     font-weight: 600;
+//     color: #737773;
+//     margin-bottom: 1rem;
+//   }
 
-  input {
-    color: #8d8e8d;
-    width: 30rem;
-    height: 3.4rem;
-    margin-bottom: 2rem;
-    border: 0;
-    background: #f6f6f6;
-    outline: none;
-    padding: 3px 5px;
-  }
+//   input {
+//     color: #8d8e8d;
+//     width: 30rem;
+//     height: 3.4rem;
+//     margin-bottom: 2rem;
+//     border: 0;
+//     background: #f6f6f6;
+//     outline: none;
+//     padding: 3px 5px;
+//   }
 
-  & > * {
-    display: block;
-    font-family: inherit;
-  }
-`;
+//   & > * {
+//     display: block;
+//     font-family: inherit;
+//   }
+// `;
 
 export default function VoucherTransaction() {
   return (
