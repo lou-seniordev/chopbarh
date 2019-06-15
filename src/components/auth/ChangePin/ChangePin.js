6a64e0679cd7c830e36022d1dc53b76a9a75a37d
topskilldev <me@topskill.dev>
2019-06-15 22:37:39 +0100

    Changed file name

---
+++ src/components/auth/ChangePin/ChangePin.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import UserHeader from "../../Dashboard/shared/UserHeader/UserHeader";
+import UserNavigation from "../../Dashboard/shared/UserNavigation/UserNavigation";
+// import EditProfileForm from "./EditProfileForm/EditProfileForm";
+import Footer from "../../UI/Footer/Footer";
+
+export default function ChangePin() {
+  return (
+    <>
+      <Helmet>
+        <title>Chopbarh &rarr; Change Pin</title>
+      </Helmet>
+      <UserHeader />
+      <UserNavigation />
+      <Footer />
+    </>
+  );
+}
