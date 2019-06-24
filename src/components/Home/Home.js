fe142448979036937a2b6e62fd9f3a76e6a01b6f
topskilldev <me@topskill.dev>
2019-06-24 23:49:50 +0100

    Removed complted todo from Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,14 +8,6 @@ import Testimonials from "./Testimonials/Testimonials";
 import GameList from "./GameList/GameList";
 import Footer from "../UI/Footer/Footer";
 
-/*
-
-This is the Landing Page for the App. Our current approach merges components
-into the same folder. We'll eventually move stuff around so it's easier to 
-reason about in the end
-
-*/
-
 export default function Home() {
   return (
     <>
