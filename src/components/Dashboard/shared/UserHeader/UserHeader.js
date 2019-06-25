34d041e8eaeca7bcdc2952511028c33b2e303826
topskilldev <me@topskill.dev>
2019-06-25 03:27:03 +0100

    Added new header for smaller screens

--- src/components/Dashboard/shared/UserHeader/UserHeader.js
+++ src/components/Dashboard/shared/UserHeader/UserHeader.js
@@ -11,6 +11,11 @@ import Icon from "../Icon/Icon";
 import CoinSymbol from "../../../assets/svg/CoinSymbol.svg";
 import VisibilityButton from "../../../assets/svg/VisibilityButton.svg";
 import CashIcon from "../../../assets/svg/CashIcon.svg";
+import Home from "../../../assets/svg/home.svg";
+import Deposit from "../../../assets/svg/Deposit.svg";
+import Withdrawal from "../../../assets/svg/Withdrawal.svg";
+import Play from "../../../assets/svg/Play.svg";
+import Transactions from "../../../assets/svg/Transaction.svg";
 import { fetchPlayerData } from "../../../../store/actions/playerDataActions";
 
 const HeaderWrapper = styled.div`
@@ -23,6 +28,7 @@ const HeaderWrapper = styled.div`
 
   a {
     font-size: 1.35rem;
+    text-decoration: none;
   }
 `;
 
@@ -92,6 +98,182 @@ class UserHeader extends Component {
                     <ul className="navbar-nav mr-auto" />
                     <ul className="navbar-nav">
                       <>
+                        <li className="nav-item">
+                          <span className="nav-link text-uppercase mr-5">
+                            <Icon icon={CoinSymbol} height="15" />
+                            {this.state.coinBalance ? (
+                              <>
+                                {new Intl.NumberFormat().format(
+                                  this.props.playerData.CBCoins
+                                )}
+                              </>
+                            ) : (
+                              <>
+                                {[
+                                  ...new Array(
+                                    Number(this.props.playerData.CBCoins)
+                                      .toString()
+                                      .split("").length
+                                  )
+                                ].map((item, id) => (
+                                  <span key={id}>*</span>
+                                ))}
+                              </>
+                            )}
+
+                            <Icon
+                              clicked={this.togglecoinBalanceVisibility}
+                              icon={VisibilityButton}
+                              height="10"
+                            />
+                            <span
+                              style={{ cursor: "pointer" }}
+                              onClick={this.props.fetchPlayerData}
+                            >
+                              <Update size="small" color="white" />
+                            </span>
+                          </span>
+                        </li>
+                        <li className="nav-item">
+                          <span className="nav-link text-uppercase mr-5">
+                            <Icon icon={CashIcon} height="18" />
+                            {this.state.cashBalance ? (
+                              <>
+                                &#8358;
+                                {new Intl.NumberFormat().format(
+                                  this.props.playerData.RealCoins
+                                )}
+                              </>
+                            ) : (
+                              <>
+                                {[
+                                  ...new Array(
+                                    Number(this.props.playerData.RealCoins)
+                                      .toString()
+                                      .split("").length
+                                  )
+                                ].map((item, id) => (
+                                  <span key={id}>*</span>
+                                ))}
+                              </>
+                            )}
+                            <Icon
+                              clicked={this.togglecashBalanceVisibility}
+                              icon={VisibilityButton}
+                              height="10"
+                            />
+                            <span
+                              style={{ cursor: "pointer" }}
+                              onClick={this.props.fetchPlayerData}
+                            >
+                              <Update size="small" color="white" />
+                            </span>
+                          </span>
+                        </li>
+                      </>
+                      <li className="nav-item dropdown">
+                        <a
+                          className="nav-link dropdown-toggle text-uppercase"
+                          href="drop"
+                          id="navbarDropdown"
+                          role="button"
+                          data-toggle="dropdown"
+                          aria-haspopup="true"
+                          aria-expanded="false"
+                        >
+                          {this.props.playerData.DisplayName ||
+                            this.props.playerData.FullName}
+                        </a>
+                        <div
+                          className="dropdown-menu dropdown-menu-right"
+                          aria-labelledby="navbarDropdown"
+                        >
+                          <Link className="dropdown-item" to="edit-profile">
+                            Profile
+                          </Link>
+                          <Link className="dropdown-item" to="change-pin">
+                            Change Pin
+                          </Link>
+                          <Link className="dropdown-item" to="logout">
+                            Logout
+                          </Link>
+                        </div>
+                      </li>
+                    </ul>
+                  </div>
+                ) : (
+                  <div
+                    className="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                  >
+                    <ul className="navbar-nav mr-auto" />
+                    <ul className="navbar-nav">
+                      <li className="nav-item">
+                        <span className="nav-link text-uppercase mr-5">
+                          Not Available
+                        </span>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </>
+            )}
+          </MediaQuery>
+          <MediaQuery maxDeviceWidth={576}>
+            {this.props.loading ? (
+              <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
+              >
+                <ul className="navbar-nav mr-auto" />
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <span className="nav-link text-uppercase mr-5">
+                      <Spinner />
+                    </span>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <>
+                {!this.props.error ? (
+                  <div
+                    className="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                  >
+                    <ul className="navbar-nav mr-auto" />
+                    <ul className="navbar-nav text-center">
+                      <>
+                        <Link className="" to="user">
+                          <Icon icon={Home} height="15" color="#fff" />
+                          <span className="nav-link text-uppercase">Home</span>
+                        </Link>
+                        <Link className="" to="user">
+                          <Icon icon={Deposit} height="15" color="#fff" />
+                          <span className="nav-link text-uppercase">
+                            Deposit
+                          </span>
+                        </Link>
+                        <Link className="" to="user">
+                          <Icon icon={Withdrawal} height="15" color="#fff" />
+                          <span className="nav-link text-uppercase">
+                            Withdraw
+                          </span>
+                        </Link>
+                        <Link className="" to="user">
+                          <Icon icon={Play} height="15" color="#fff" />
+                          <span className="nav-link text-uppercase">Play</span>
+                        </Link>
+                        <Link className="" to="user">
+                          <Icon icon={Transactions} height="15" color="#fff" />
+                          <span className="nav-link text-uppercase">
+                            Transactions
+                          </span>
+                        </Link>
+                        <Link className="" to="user">
+                          <Icon icon={Home} height="15" color="#fff" />
+                          <span className="nav-link text-uppercase">Home</span>
+                        </Link>
                         <li className="nav-item">
                           <span className="nav-link text-uppercase mr-5">
                             <Icon icon={CoinSymbol} height="15" />
