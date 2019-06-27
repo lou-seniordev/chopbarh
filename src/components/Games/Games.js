5ce371d874c0e188f4ef4552b90049ef9cc4c390
topskilldev <me@topskill.dev>
2019-06-27 12:38:54 +0100

    Added new component to Games page

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -3,7 +3,8 @@ import { Helmet } from "react-helmet";
 import Header from "../UI/Header/Header";
 import Footer from "../UI/Footer/Footer";
 import GamesHero from "./GamesHero/GamesHero";
-import GameItems from "./GameItems/GameItems";
+// import GameItems from "./GameItems/GameItems";
+import QuickPlayTransaction from "../Dashboard/UserTransaction/QuickPlayTransaction/QuickPlayTransaction";
 
 export default function Games() {
   return (
@@ -11,7 +12,13 @@ export default function Games() {
       <Helmet title={`Chopbarh \u{2192} Games`} />
       <Header />
       <GamesHero />
-      <GameItems />
+      <div className="container" style={{ minHeight: "70vh" }}>
+        <div className="row">
+          <div className="col-md-12">
+            <QuickPlayTransaction />
+          </div>
+        </div>
+      </div>
       <Footer />
     </>
   );
