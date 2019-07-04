c3b74a9731d312f1a5221b4c1d67944cd7175ebe
topskilldev <me@topskill.dev>
2019-07-04 13:05:10 +0100

    Revert lifecycle logic in DepositTable

--- src/components/Dashboard/UserTransaction/DepositTable/DepositTable.js
+++ src/components/Dashboard/UserTransaction/DepositTable/DepositTable.js
@@ -10,9 +10,9 @@ const DepositTableWrapper = styled.div`
 
 class DepositTable extends Component {
   componentDidMount = () => {
-    if (!this.props.depositData) {
-      this.props.fetchDepositHistoryData();
-    }
+    this.props.fetchDepositHistoryData();
+    // if (!this.props.depositData) {
+    // }
   };
 
   render() {
@@ -20,48 +20,48 @@ class DepositTable extends Component {
       <DepositTableWrapper>
         {this.props.depositData ? (
           <div className="table-responsive">
-          <table className="table table-striped">
-            <thead
-              style={{
-                background: "#8C1936",
-                color: "#fff",
-                textAlign: "center"
-              }}
-            >
-              <tr>
-                <th scope="col">Status</th>
-                <th scope="col">Reference</th>
-                <th scope="col">Transaction Date</th>
-                <th scope="col">Transaction Time</th>
-                <th scope="col">Transaction Fees</th>
-                <th scope="col">Amount</th>
-                <th scope="col">Channel</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.props.depositData.map((deposit, index) => (
-                <tr key={index} style={{ textAlign: "center" }}>
-                  <td>{deposit.status}</td>
-                  <td>{deposit.transaction_reference}</td>
-                  <td>{`${new Date(
-                    deposit.deposit_date
-                  ).toLocaleDateString()}`}</td>
-                  <td>{`${new Date(
-                    deposit.deposit_date
-                  ).toLocaleTimeString()}`}</td>
-                  <td>{deposit.transaction_fees}</td>
-                  <td>
-                    &#8358;
-                    {new Intl.NumberFormat().format(deposit.amount)}
-                  </td>
-                  <td>
-                    {deposit.channel.charAt(0).toUpperCase() +
-                      deposit.channel.slice(1)}
-                  </td>
+            <table className="table table-striped">
+              <thead
+                style={{
+                  background: "#8C1936",
+                  color: "#fff",
+                  textAlign: "center"
+                }}
+              >
+                <tr>
+                  <th scope="col">Status</th>
+                  <th scope="col">Reference</th>
+                  <th scope="col">Transaction Date</th>
+                  <th scope="col">Transaction Time</th>
+                  <th scope="col">Transaction Fees</th>
+                  <th scope="col">Amount</th>
+                  <th scope="col">Channel</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody>
+                {this.props.depositData.map((deposit, index) => (
+                  <tr key={index} style={{ textAlign: "center" }}>
+                    <td>{deposit.status}</td>
+                    <td>{deposit.transaction_reference}</td>
+                    <td>{`${new Date(
+                      deposit.deposit_date
+                    ).toLocaleDateString()}`}</td>
+                    <td>{`${new Date(
+                      deposit.deposit_date
+                    ).toLocaleTimeString()}`}</td>
+                    <td>{deposit.transaction_fees}</td>
+                    <td>
+                      &#8358;
+                      {new Intl.NumberFormat().format(deposit.amount)}
+                    </td>
+                    <td>
+                      {deposit.channel.charAt(0).toUpperCase() +
+                        deposit.channel.slice(1)}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         ) : (
           <>
