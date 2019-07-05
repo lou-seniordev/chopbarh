b7cdb2f7c15fed468e0d483e31342f8d452090a2
topskilldev <me@topskill.dev>
2019-07-05 11:37:16 +0100

    Added new link to notfound page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 import breakpoint from "../styles/breakpoints";
+import color from "../styles/colors";
 
 const NotFoundWrapper = styled.div`
   height: 100vh;
@@ -32,6 +34,23 @@ const NotFoundWrapper = styled.div`
       font-size: 1.5rem;
     }
   }
+
+  a {
+    margin: 2rem auto;
+    display: block;
+    background: ${color.colorPrimary};
+    color: #fff;
+    text-transform: uppercase;
+    text-decoration: none;
+    width: 13rem;
+    padding: 0.5rem;
+    transition: all 0.3s;
+
+    &:hover {
+      background: ${color.colorPrimaryHover};
+      transform: translateY(-3px);
+    }
+  }
 `;
 
 export default function NotFound() {
@@ -40,6 +59,7 @@ export default function NotFound() {
       <div>
         <h3>Ooops!</h3>
         <p>Page not found</p>
+        <Link to="/">Go to Homepage</Link>
       </div>
     </NotFoundWrapper>
   );
