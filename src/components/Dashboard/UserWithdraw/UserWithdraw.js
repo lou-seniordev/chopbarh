8c8e6a756e38bb5632f1aa99aaaf96c6ff984ce6
topskilldev <me@topskill.dev>
2019-07-09 14:40:41 +0100

    Added connect to UserWithdraw wrapper component

--- src/components/Dashboard/UserWithdraw/UserWithdraw.js
+++ src/components/Dashboard/UserWithdraw/UserWithdraw.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react";
 import { Helmet } from "react-helmet";
 import MediaQuery from "react-responsive";
 import UserHeader from "../shared/UserHeader/UserHeader";
@@ -7,34 +8,31 @@ import UserNavigation from "../shared/UserNavigation/UserNavigation";
 import WithdrawTabs from "./WithdrawTabs/WithdrawTabs";
 import WithdrawSmallScreens from "./WithdrawSmallScreens/WithdrawSmallScreens";
 
-export default function UserDeposit() {
-  return (
-    <>
-      <Helmet title={`Chopbarh \u{2192} Withdraw`} />
-      <UserHeader />
-      <UserNavigation />
-      <div style={{ minHeight: "80vh" }}>
-        <MediaQuery minDeviceWidth={767}>
-          <WithdrawTabs />
-        </MediaQuery>
-        <MediaQuery maxDeviceWidth={767}>
-          <WithdrawSmallScreens />
-        </MediaQuery>
-      </div>
-      {/* <div className="container">
-        <p>
-          Note: No. We are not sending you airtime! You can withdraw by dialing
-          *242# on your phone number Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Purus gravida quis blandit turpis cursus in hac
-          habitasse. Auctor urna nunc id cursus metus aliquam. Laoreet id donec
-          ultrices tincidunt arcu non. Sed pulvinar proin gravida hendrerit.
-          Adipiscing at in tellus integer feugiat scelerisque. Egestas congue
-          quisque egestas diam in. Nec nam aliquam sem et tortor consequat id
-          porta nibh. Montes nascetur ridiculus mus mauris vitae.
-        </p>
-      </div> */}
-      <Footer />
-    </>
-  );
+class UserWithdraw extends Component {
+  render() {
+    return (
+      <>
+        <Helmet title={`Chopbarh \u{2192} Withdraw`} />
+        <UserHeader />
+        <UserNavigation />
+        <div style={{ minHeight: "80vh" }}>
+          <MediaQuery minDeviceWidth={767}>
+            <WithdrawTabs />
+          </MediaQuery>
+          <MediaQuery maxDeviceWidth={767}>
+            <WithdrawSmallScreens />
+          </MediaQuery>
+        </div>
+        <div className="container">
+          <p>Withdrawal Status</p>
+        </div>
+        <Footer />
+      </>
+    );
+  }
 }
+
+export default connect(
+  null,
+  null
+)(UserWithdraw);
