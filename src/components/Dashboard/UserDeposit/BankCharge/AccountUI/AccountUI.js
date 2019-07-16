c61bd731cc45d48cb55b8b334d10eb8a3f0a93bc
topskilldev <me@topskill.dev>
2019-07-16 04:03:05 +0100

    Added UI logic to AccountUI component

---
+++ src/components/Dashboard/UserDeposit/BankCharge/AccountUI/AccountUI.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background: #eee;
+  padding: 0px 10px;
+  border-radius: 5px;
+  min-width: 26rem;
+  margin: 14px;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 37px;
+
+  p {
+    margin-bottom: 0;
+  }
+`;
+
+const CardComponent = styled.div`
+  position: relative;
+  margin-right: 1rem;
+`;
+
+const CardComponentHeader = styled.p`
+  position: absolute;
+  top: -29px;
+  width: 9rem;
+`;
+
+export default class CreditCard extends Component {
+  render() {
+    return (
+      <Container>
+        <CardComponent>
+          <CardComponentHeader>Account Number</CardComponentHeader>
+          <p>XXXXX X{this.props.number}</p>
+        </CardComponent>
+        <CardComponent>
+          <CardComponentHeader>Bank</CardComponentHeader>
+          <p>{this.props.bank}</p>
+        </CardComponent>
+      </Container>
+    );
+  }
+}
