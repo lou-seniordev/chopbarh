219ca53d61709825fd6faf34eb4b993cbe525706
topskilldev <me@topskill.dev>
2019-07-16 04:29:18 +0100

    Added bank account removal actions

--- src/store/actions/bankAccountActions.js
+++ src/store/actions/bankAccountActions.js
@@ -105,3 +105,59 @@ export const setBankAccountData = payload => async (dispatch, getState) => {
     dispatch(setBankAccountFail());
   }
 };
+
+export const removeBankAccountInit = () => ({
+  type: actionType.REMOVE_BANK_ACCOUNT_INIT
+});
+
+export const removeBankAccountSuccess = () => ({
+  type: actionType.REMOVE_BANK_ACCOUNT_SUCCESS
+});
+
+export const removeBankAccountFail = () => ({
+  type: actionType.REMOVE_BANK_ACCOUNT_FAIL
+});
+
+export const removeBankAccount = (event, authCode) => async (
+  dispatch,
+  getState
+) => {
+  dispatch(removeBankAccountInit());
+
+  try {
+    const snapshot = await firestore
+      .collection("bank_charge")
+      .where("id", "==", getState().auth.id)
+      .get();
+
+    const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    // console.log(data);
+    if (data.length) {
+      // dispatch(fetchBankAccountSuccess(data[0].data));
+      const accountsArray = data[0].data;
+      let filteredArray = accountsArray.filter(
+        account => account.auth_code !== authCode
+      );
+
+      try {
+        const docRef = await firestore
+          .collection("bank_charge")
+          .doc(getState().auth.id)
+          .update({
+            data: filteredArray
+          });
+        dispatch(removeBankAccountSuccess(docRef));
+      } catch (err) {
+        toast.error("Account could not be removed. Please try again");
+      }
+      window.location.reload();
+      dispatch(fetchBankAccountData());
+      toast.success("Account was removed");
+    } else {
+      // dispatch(fetchBankAccountFail());
+    }
+  } catch (err) {
+    console.log("Error...", err);
+    dispatch(removeBankAccountFail());
+  }
+};
