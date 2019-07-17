9dcc12ff8650e67cc42e265004450af0a10d5acd
topskilldev <me@topskill.dev>
2019-07-17 10:10:01 +0100

    Added new reducer file

---
+++ src/store/reducers/withdrawalAccountReducer.js
@@ -0,0 +1,53 @@
+import * as actionType from "../actionTypes/actionTypes";
+
+const initialState = {
+  loading: true,
+  withdrawalAccount: [],
+  error: false,
+  removing: false
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case actionType.FETCH_WITHDRAWAL_BANK_ACCOUNT_INIT:
+      return {
+        ...state,
+        loading: true
+      };
+    case actionType.FETCH_WITHDRAWAL_BANK_ACCOUNT_SUCCESS:
+      return {
+        ...state,
+        withdrawalAccount: state.withdrawalAccount.concat(action.data),
+        loading: false
+      };
+    case actionType.FETCH_WITHDRAWAL_BANK_ACCOUNT_FAIL:
+      return {
+        ...state,
+        loading: false,
+        error: true
+      };
+    case actionType.REMOVE_WITHDRAWAL_BANK_ACCOUNT_INIT:
+      return {
+        ...state,
+        removing: true
+      };
+    case actionType.REMOVE_WITHDRAWAL_BANK_ACCOUNT_SUCCESS:
+      return {
+        ...state,
+        removing: false
+      };
+    case actionType.REMOVE_WITHDRAWAL_BANK_ACCOUNT_FAIL:
+      return {
+        ...state,
+        removing: false
+      };
+    case actionType.AUTH_LOGOUT:
+      return {
+        ...state,
+        loading: true,
+        withdrawalAccount: null
+      };
+    default:
+      return state;
+  }
+};
