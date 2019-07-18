c8131f47ce79dd3f5c0448d9f0e400e67fbc5f71
topskilldev <me@topskill.dev>
2019-07-18 15:26:22 +0100

    Added new case to SubmitOTP logic

--- src/components/Dashboard/UserDeposit/Card/SubmitOTP/SubmitOTP.js
+++ src/components/Dashboard/UserDeposit/Card/SubmitOTP/SubmitOTP.js
@@ -74,14 +74,17 @@ class SubmitOTP extends Component {
           ...data.data.authorization,
           cvv: this.props.cvv
         };
-        const historyObject = {
-          ...data.data,
-          fees: +data.data.amount / 100 < 2500 ? 0 : 100
-        };
+        // const historyObject = {
+        //   ...data.data,
+        //   fees: +data.data.amount / 100 < 2500 ? 0 : 100
+        // };
         // const value = +data.data.amount / 100;
-        this.props.setDepositHistory(historyObject);
+        // this.props.setDepositHistory(historyObject);
         // this.props.setCoinBalance(value);
         this.props.setCreditCardData(payload);
+      } else if (data.data.status === "open_url") {
+        this.props.closeOTPModal();
+        window.open(data.data.url, "_blank");
       } else {
         toast.error(`Please try again`);
         this.setState({ loading: false });
