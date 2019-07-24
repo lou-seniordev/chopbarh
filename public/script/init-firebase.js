ac43b8337bf76c4c5313faa82e7c8bd311c5ed97
topskilldev <me@topskill.dev>
2019-07-25 00:31:47 +0100

    Added Firebase Performance Monitoring

---
+++ public/script/init-firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAZcOFJZlGM3etCczLDYNkFMW8ty0wSduU",
+  authDomain: "chopbarh-website.firebaseapp.com",
+  databaseURL: "https://chopbarh-website.firebaseio.com",
+  projectId: "chopbarh-website",
+  storageBucket: "chopbarh-website.appspot.com",
+  messagingSenderId: "904298439795",
+  appId: "1:904298439795:web:eb5be126202c5d24"
+};
+
