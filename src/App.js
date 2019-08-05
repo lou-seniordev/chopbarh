1c41060cf7377f09f2c37c324f7c10bca1968b4d
topskilldev <me@topskill.dev>
2019-08-05 16:09:09 +0100

    Added new style prop

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Layout from "./container/Layout";
 import ErrorBoundary from "./hoc/ErrorBoundary";
 import { GlobalStyles } from "./components/styles/GlobalStyles";
 import "react-toastify/dist/ReactToastify.css";
-import './App.css'
+import { Reset } from "styled-reset";
+import "./App.css";
 
 export default function App() {
   return (
     <ErrorBoundary>
       <ToastContainer />
+      <Reset />
       <GlobalStyles />
       <Layout />
     </ErrorBoundary>
