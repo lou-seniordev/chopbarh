a6274f60d8e82a28fdfb3c39a353defbc1a7c232
topskilldev <me@topskill.dev>
2019-08-21 22:28:51 +0100

    Added new reducer file

--- src/store/reducers/raveCardReducer.js
+++ src/store/reducers/raveCardReducer.js
@@ -2,7 +2,7 @@ import * as actionType from "../actionTypes/actionTypes";
 
 const initialState = {
   loading: true,
-  creditCard: [],
+  raveCard: [],
   cvv: null,
   fetched: false,
   removing: false,
@@ -11,46 +11,31 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case actionType.FETCH_CREDIT_CARD_INIT:
+    case actionType.FETCH_RAVE_CARD_INIT:
       return {
         ...state,
         loading: true
       };
-    case actionType.FETCH_CREDIT_CARD_SUCCESS:
+    case actionType.FETCH_RAVE_CARD_SUCCESS:
       return {
         ...state,
-        creditCard: [...action.data],
+        raveCard: [...action.data],
         loading: false,
         fetched: true
       };
-    case actionType.FETCH_CREDIT_CARD_FAIL:
+    case actionType.FETCH_RAVE_CARD_FAIL:
       return {
         ...state,
         loading: false,
         error: true
       };
-    case actionType.SET_CREDIT_CARD_SUCCESS:
-      return {
-        ...state,
-        creditCard: [...action.data],
-        loading: false
-      };
-    case actionType.SET_CREDIT_CARD_CVV:
-      return {
-        ...state,
-        cvv: action.cvv
-      };
-    case actionType.REMOVE_CREDIT_CARD_INIT:
+
+    case actionType.REMOVE_RAVE_CARD_INIT:
       return {
         ...state,
         removing: true
       };
-    case actionType.REMOVE_CREDIT_CARD_SUCCESS:
-      return {
-        ...state,
-        removing: false
-      };
-    case actionType.SET_BANK_ACCOUNT_FAIL:
+    case actionType.REMOVE_RAVE_CARD_SUCCESS:
       return {
         ...state,
         removing: false
@@ -59,7 +44,7 @@ export default (state = initialState, action) => {
       return {
         ...state,
         loading: true,
-        creditCard: []
+        raveCard: []
       };
     default:
       return state;
