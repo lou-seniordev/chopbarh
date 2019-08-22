3f0f3b613197da368345410167b127b5326eeaae
topskilldev <me@topskill.dev>
2019-08-22 12:16:35 +0100

    Added new Rave keys

--- src/components/Dashboard/UserDeposit/Rave/Rave.js
+++ src/components/Dashboard/UserDeposit/Rave/Rave.js
@@ -29,13 +29,14 @@ import "react-accessible-accordion/dist/fancy-example.css";
 
 class RavePayment extends Component {
   state = {
-    key: "FLWPUBK_TEST-195cdc10fea3cdfc1be0d60cf6aa0c80-X",
+    key: "FLWPUBK-d1914cca4535e30998a1289ca01a50b1-X",
     email: "chopbarh@mail.com",
     amount: "",
     selectedValue: null,
     removeRaveCardModal: false,
     authAmount: "",
-    last4Digits: ""
+    last4Digits: "",
+    paying: false
   };
 
   // key: "FLWPUBK-d1914cca4535e30998a1289ca01a50b1-X",
@@ -121,45 +122,61 @@ class RavePayment extends Component {
       card => card.auth_code === this.state.selectedValue
     );
 
-    console.log(raveCardObject);
+    // console.log(raveCardObject);
 
-    if (raveCardObject[0].last_digits !== this.state.last4Digits) {
-      // this.setState({ paying: false });
-      toast.error(`Last 4 digits is not correct`);
-      return;
-    }
+    // if (raveCardObject[0].last_digits !== this.state.last4Digits) {
+    //   // this.setState({ paying: false });
+    //   toast.error(`Last 4 digits is not correct`);
+    //   return;
+    // }
 
     let reference = this.getReference();
 
-    // try {
-    //   const historyObject = {
-    //     amount: this.state.authAmount,
-    //     channel: "Card",
-    //     transaction_date: new Date().toISOString(),
-    //     fees: "None",
-    //     reference: "--",
-    //     status: "--",
-    //     refId: `${this.props.playerData.PhoneNum}-${reference}`,
-    //     gateway: "Flutterwave",
-    //     made_by: this.props.playerData.PhoneNum
-    //   };
+    try {
+      this.setState({ paying: true });
 
-    //   this.props.setDepositHistory(historyObject);
+      const historyObject = {
+        amount: this.state.authAmount,
+        channel: "Card",
+        transaction_date: new Date().toISOString(),
+        fees: "None",
+        reference: "--",
+        status: "--",
+        refId: `${this.props.playerData.PhoneNum}-${reference}`,
+        gateway: "Flutterwave",
+        made_by: this.props.playerData.PhoneNum
+      };
 
-    //   const response = await fetch('https://ravesandboxapi.flutterwave.com/flwv3-pug/getpaidx/api/tokenized/charge',
-    //   {method: 'POST', headers: {
-    //     'Content-Type': 'application/json'
-    //   }, body: JSON.stringify({
-    //     SECKEY: '',
-    //     token: '',
-    //     currency: 'NG',
-    //     amount: this.state.authAmount,
-    //     email: `${this.props.playerData.PhoneNum}@mail.com`,
-    //     txRef: `${this.props.playerData.PhoneNum}-${reference}`
-    //   })})
-    // } catch (err) {
+      this.props.setDepositHistory(historyObject);
 
-    // }
+      const response = await fetch(
+        "https://api.ravepay.co/flwv3-pug/getpaidx/api/tokenized/charge",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            SECKEY: "FLWSECK-152efa07e12758633c4da1be7a0067c4-X",
+            token: raveCardObject[0].auth_code,
+            currency: "NG",
+            amount: this.state.authAmount,
+            email: `${this.props.playerData.PhoneNum}@mail.com`,
+            txRef: `${this.props.playerData.PhoneNum}-${reference}`
+          })
+        }
+      );
+
+      const data = await response.json();
+
+      if (data.status === "success") {
+        this.setState({ paying: false });
+        toast.info("Transaction is processing");
+      } else {
+        this.setState({ paying: false });
+        toast.error("Transaction not successful");
+      }
+    } catch (err) {}
 
     // Fetch Flutterwave here
   };
@@ -307,7 +324,7 @@ class RavePayment extends Component {
                                 number={card.last_digits}
                                 expiry={card.expiry}
                               />
-                              <ExistingCardFormItem>
+                              {/* <ExistingCardFormItem>
                                 <input
                                   onChange={this.handleInputChange}
                                   className="mt-lg-4 mt-md-3"
@@ -323,7 +340,7 @@ class RavePayment extends Component {
                                   minLength="1"
                                   placeholder="Last 4 Card Digits"
                                 />
-                              </ExistingCardFormItem>
+                              </ExistingCardFormItem> */}
                               <Button
                                 id="Popover"
                                 type="button"
@@ -351,10 +368,10 @@ class RavePayment extends Component {
                         <FormSubmitButton
                           type="submit"
                           className="mr-2 mt-n4"
-                          disabled={this.state.loading}
+                          disabled={this.state.paying}
                         >
                           <span>
-                            {this.state.loading ? "Please wait..." : "Load"}
+                            {this.state.paying ? "Please wait..." : "Load"}
                           </span>
                         </FormSubmitButton>
                       </ExistingCardForm>
