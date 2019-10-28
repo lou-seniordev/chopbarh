d830309b37741eb70ecb8f5ac33a5871959eb24e
topskilldev <me@topskill.dev>
2019-10-28 11:29:35 +0700

    Added new cloud function for authentication

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -5,62 +5,55 @@ import breakpoint from "../styles/breakpoints";
 import color from "../styles/colors";
 
 const NotFoundWrapper = styled.div`
-  height: 100vh;
-  text-align: center;
-  padding: 50vh 0;
-  position: relative;
+	height: 100vh;
+	text-align: center;
+	padding: 50vh 0;
+	position: relative;
 
-  h3 {
-    text-transform: uppercase;
-    font-weight: 300;
-    font-size: 6rem;
+	h3 {
+		text-transform: uppercase;
+		font-weight: 300;
+		font-size: 7rem;
 
-    @media screen and (max-width: ${breakpoint.medium}) {
-      font-size: 5.5rem;
-    }
-  }
+		@media screen and (max-width: ${breakpoint.medium}) {
+			font-size: 5.5rem;
+		}
+	}
 
-  p {
-    background: #fff;
-    padding: 0.5rem;
-    display: inline-block;
-    position: absolute;
-    font-size: 1.8rem;
-    top: 60vh;
-    transform: translate(-50%, -50%);
+	span {
+		background: #fff;
+		padding: -3.5rem;
+		font-size: 1.8rem;
+	}
 
-    @media screen and (max-width: ${breakpoint.medium}) {
-      top: 62vh;
-      font-size: 1.5rem;
-    }
-  }
+	a {
+		margin: 1rem auto;
+		display: block;
+		background: ${color.colorPrimary};
+		color: #fff;
+		text-transform: uppercase;
+		text-decoration: none;
+		width: 13rem;
+		padding: 0.5rem;
+		transition: all 0.3s;
 
-  a {
-    margin: 2rem auto;
-    display: block;
-    background: ${color.colorPrimary};
-    color: #fff;
-    text-transform: uppercase;
-    text-decoration: none;
-    width: 13rem;
-    padding: 0.5rem;
-    transition: all 0.3s;
-
-    &:hover {
-      background: ${color.colorPrimaryHover};
-      transform: translateY(-3px);
-    }
-  }
+		&:hover {
+			background: ${color.colorPrimaryHover};
+			transform: translateY(-3px);
+		}
+	}
 `;
 
 export default function NotFound() {
-  return (
-    <NotFoundWrapper>
-      <div>
-        <h3>Ooops!</h3>
-        <p>Page not found</p>
-        <Link to="/">Go to Homepage</Link>
-      </div>
-    </NotFoundWrapper>
-  );
+	return (
+		<NotFoundWrapper>
+			<div>
+				<h3>
+					Ooops! <br />
+					<span>Page not found</span>
+				</h3>
+				<Link to="/">Go to Homepage</Link>
+			</div>
+		</NotFoundWrapper>
+	);
 }
--- src/hoc/ErrorBoundary.js
+++ src/hoc/ErrorBoundary.js
@@ -5,81 +5,74 @@ import breakpoint from "../components/styles/breakpoints";
 import color from "../components/styles/colors";
 
 const ErrorBoundaryWrapper = styled.div`
-  height: 100vh;
-  text-align: center;
-  padding: 50vh 0;
-  position: relative;
+	height: 100vh;
+	text-align: center;
+	padding: 50vh 0;
+	position: relative;
 
-  h3 {
-    text-transform: uppercase;
-    font-weight: 300;
-    font-size: 6rem;
+	h3 {
+		text-transform: uppercase;
+		font-weight: 300;
+		font-size: 6rem;
 
-    @media screen and (max-width: ${breakpoint.medium}) {
-      font-size: 5.5rem;
-    }
-  }
+		@media screen and (max-width: ${breakpoint.medium}) {
+			font-size: 5.5rem;
+		}
+	}
 
-  p {
-    background: #fff;
-    padding: 0.5rem;
-    display: inline-block;
-    position: absolute;
-    font-size: 1.8rem;
-    top: 66vh;
-    transform: translate(-50%, -50%);
+	span {
+		background: #fff;
+		padding: 0.5rem;
+		font-size: 1.8rem;
+	}
 
-    @media screen and (max-width: ${breakpoint.medium}) {
-      top: 68vh;
-      font-size: 1.5rem;
-    }
-  }
+	a {
+		margin: 3rem auto;
+		display: block;
+		background: ${color.colorPrimary};
+		color: #fff;
+		text-transform: uppercase;
+		text-decoration: none;
+		width: 13rem;
+		padding: 0.5rem;
+		transition: all 0.3s;
 
-  a {
-    margin: 3rem auto;
-    display: block;
-    background: ${color.colorPrimary};
-    color: #fff;
-    text-transform: uppercase;
-    text-decoration: none;
-    width: 13rem;
-    padding: 0.5rem;
-    transition: all 0.3s;
-
-    &:hover {
-      background: ${color.colorPrimaryHover};
-      transform: translateY(-3px);
-    }
-  }
+		&:hover {
+			background: ${color.colorPrimaryHover};
+			transform: translateY(-3px);
+		}
+	}
 `;
 
 export default class ErrorBoundary extends Component {
-  state = {
-    error: null,
-    errorModal: false
-  };
+	state = {
+		error: null,
+		errorModal: false
+	};
 
-  componentDidCatch = error => {
-    this.setState({ error, errorModal: true });
-  };
+	componentDidCatch = error => {
+		this.setState({ error, errorModal: true });
+	};
 
-  render() {
-    const { error } = this.state;
+	render() {
+		const { error } = this.state;
 
-    if (error) {
-      return (
-        <ErrorBoundaryWrapper>
-          <div>
-            <h3>Ooops!</h3>
-            <p>An Error Occured!</p>
-            <Link to="/" onClick={this.forceUpdate}>
-              Go to Homepage
-            </Link>
-          </div>
-        </ErrorBoundaryWrapper>
-      );
-    }
+		if (error) {
+			return (
+				<ErrorBoundaryWrapper>
+					<div>
+						<h3>
+							Ooops!
+							<span>An Error Occured!</span>
+						</h3>
+						<Link to="/" onClick={this.forceUpdate}>
+							Go to Homepage
+						</Link>
+					</div>
+				</ErrorBoundaryWrapper>
+			);
+		}
 
-    return <div>{this.props.children}</div>;
-  }
+		return <div>{this.props.children}</div>;
+	}
 }
