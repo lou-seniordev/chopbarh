8b240c8b09efd7789e0eab2c0b9937220077172c
topskilldev <me@topskill.dev>
2019-11-01 12:17:11 +0700

    Adjusted Paystack Card Styling

--- src/components/Dashboard/UserDeposit/Card/Card.js
+++ src/components/Dashboard/UserDeposit/Card/Card.js
@@ -766,7 +766,7 @@ class Card extends Component {
             )}
           </>
         )}
-        <div className="text-center" style={{ color: "#000" }}>
+        <div className="text-center mt-4" style={{ color: "#000" }}>
           <p>
             **For deposits of &#8358;2,500 and above, there is a &#8358;100
             charge added to the deposit amount**
