174387f2e6fc26e5f41bfa504680dc7a1a4a2b86
topskilldev <me@topskill.dev>
2019-11-26 14:48:08 +0700

    Added super agent application route to HOC

--- src/container/Layout.js
+++ src/container/Layout.js
@@ -12,7 +12,7 @@ const SignUpPage = lazy(() => import("../Pages/SignUpPage"));
 const ChangePinPage = lazy(() => import("../Pages/ChangePinPage"));
 // const CompleteProfilePage = lazy(() => import("../Pages/CompleteProfilePage"));
 const ForgotPassword = lazy(() =>
-	import("../components/auth/ForgotPassword/ForgotPassword")
+  import("../components/auth/ForgotPassword/ForgotPassword")
 );
 const Logout = lazy(() => import("../components/auth/Logout/Logout"));
 // const SetNickname = lazy(() =>
@@ -26,101 +26,107 @@ const UserDepositPage = lazy(() => import("../Pages/UserDepositPage"));
 const UserWithdrawPage = lazy(() => import("../Pages/UserWithdrawPage"));
 const UserTransactionPage = lazy(() => import("../Pages/UserTransactionPage"));
 const VendorsPage = lazy(() => import("../Pages/VendorsPage"));
+const SuperAgentApplicationPage = lazy(() =>
+  import("../Pages/SuperAgentApplicationPage")
+);
 const NotFoundPage = lazy(() => import("../Pages/NotFoundPage"));
 
 const Loading = () => (
-	<div
-		style={{
-			height: "98vh",
-			display: "flex",
-			alignItems: "center",
-			justifyContent: "center"
-		}}
-	>
-		<Spinner type="grow" style={{ color: "#6D0A23" }} />
-	</div>
+  <div
+    style={{
+      height: "98vh",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    }}
+  >
+    <Spinner type="grow" style={{ color: "#6D0A23" }} />
+  </div>
 );
 
 class Layout extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.props.isAuthenticated &&
-			this.props.authSuccess(
-				localStorage.getItem("chopbarh-token:live"),
-				localStorage.getItem("chopbarh-id:live")
-			);
-	}
+    this.props.isAuthenticated &&
+      this.props.authSuccess(
+        localStorage.getItem("chopbarh-token:live"),
+        localStorage.getItem("chopbarh-id:live")
+      );
+  }
 
-	// UNSAFE_componentWillMount = () => {
-	//   if (this.props.isAuthenticated) {
-	//     this.props.authSuccess(
-	//       localStorage.getItem("chopbarh-token:live"),
-	//       localStorage.getItem("chopbarh-id:live")
-	//     );
-	//     //this.props.fetchPlayerData();
-	//   }
-	//   // if (localStorage.getItem("chopbarh-token:live")) {
-	//   //   localStorage.removeItem("chopbarh-token:live");
-	//   //   localStorage.removeItem("chopbarh-id:live");
-	//   //   this.props.history.replace("/");
-	//   // }
-	// };
+  // UNSAFE_componentWillMount = () => {
+  //   if (this.props.isAuthenticated) {
+  //     this.props.authSuccess(
+  //       localStorage.getItem("chopbarh-token:live"),
+  //       localStorage.getItem("chopbarh-id:live")
+  //     );
+  //     //this.props.fetchPlayerData();
+  //   }
+  //   // if (localStorage.getItem("chopbarh-token:live")) {
+  //   //   localStorage.removeItem("chopbarh-token:live");
+  //   //   localStorage.removeItem("chopbarh-id:live");
+  //   //   this.props.history.replace("/");
+  //   // }
+  // };
 
-	render() {
-		return (
-			<>
-				{this.props.isAuthenticated ? (
-					<Suspense fallback={Loading()}>
-						<Switch>
-							<Route path="/" exact component={LandingPage} />
-							<Route path="/games" component={GamesPage} />
-							<Route path="/reset" component={ForgotPassword} />
-							<Route path="/logout" component={Logout} />
-							<Route path="/user" component={UserHomePage} />
-							<Route path="/profile" component={UserProfilePage} />
-							<Route path="/edit-profile" component={UserEditProfilePage} />
-							<Route path="/change-pin" component={ChangePinPage} />
-							<Route path="/deposit" component={UserDepositPage} />
-							<Route path="/withdraw" component={UserWithdrawPage} />
-							<Route path="/play" component={UserPlayPage} />
-							<Route path="/transaction" component={UserTransactionPage} />
-							<Route path="/vendors" component={VendorsPage} />
-							<Route path="*" component={NotFoundPage} />
-						</Switch>
-					</Suspense>
-				) : (
-					<Suspense fallback={Loading()}>
-						<Switch>
-							<Route path="/" exact component={LandingPage} />
-							<Route path="/games" component={GamesPage} />
-							<Route path="/login" component={LoginPage} />
-							<Route path="/signup" component={SignUpPage} />
-							<Route path="/vendors" component={VendorsPage} />
-							{/* <Route path="/set-nickname" component={SetNickname} />
+  render() {
+    return (
+      <>
+        {this.props.isAuthenticated ? (
+          <Suspense fallback={Loading()}>
+            <Switch>
+              <Route path="/" exact component={LandingPage} />
+              <Route path="/games" component={GamesPage} />
+              <Route path="/reset" component={ForgotPassword} />
+              <Route path="/logout" component={Logout} />
+              <Route path="/user" component={UserHomePage} />
+              <Route path="/profile" component={UserProfilePage} />
+              <Route path="/edit-profile" component={UserEditProfilePage} />
+              <Route path="/change-pin" component={ChangePinPage} />
+              <Route path="/deposit" component={UserDepositPage} />
+              <Route path="/withdraw" component={UserWithdrawPage} />
+              <Route path="/play" component={UserPlayPage} />
+              <Route path="/transaction" component={UserTransactionPage} />
+              <Route path="/vendors" component={VendorsPage} />
+              <Route
+                path="/super-agent-application"
+                component={SuperAgentApplicationPage}
+              />
+              <Route path="*" component={NotFoundPage} />
+            </Switch>
+          </Suspense>
+        ) : (
+          <Suspense fallback={Loading()}>
+            <Switch>
+              <Route path="/" exact component={LandingPage} />
+              <Route path="/games" component={GamesPage} />
+              <Route path="/login" component={LoginPage} />
+              <Route path="/signup" component={SignUpPage} />
+              <Route path="/vendors" component={VendorsPage} />
+              <Route
+                path="/super-agent-application"
+                component={SuperAgentApplicationPage}
+              />
+              {/* <Route path="/set-nickname" component={SetNickname} />
               <Route path="/complete_profile" component={CompleteProfilePage} /> */}
 
-							<Redirect push to="/login" />
-						</Switch>
-					</Suspense>
-				)}
-			</>
-		);
-	}
+              <Redirect push to="/login" />
+            </Switch>
+          </Suspense>
+        )}
+      </>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-	isAuthenticated: localStorage.getItem("chopbarh-token:live") !== null
+  isAuthenticated: localStorage.getItem("chopbarh-token:live") !== null
 });
 
 const mapDispatchToProps = {
-	authSuccess,
-	fetchPlayerData
+  authSuccess,
+  fetchPlayerData
 };
 
-export default withRouter(
-	connect(
-		mapStateToProps,
-		mapDispatchToProps
-	)(Layout)
-);
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Layout));
