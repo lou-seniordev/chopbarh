531e65f69c3b8f8de5437708638b034780c3c026
topskilldev <me@topskill.dev>
2019-11-26 14:46:21 +0700

    Created and added base content to SuperAgentApplication page

---
+++ src/components/SuperAgentApplication/SuperAgentApplication.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SuperAgentApplication() {
+  return (
+    <div className="text-center">
+      <h3>Super Agent Application</h3>
+    </div>
+  );
+}
