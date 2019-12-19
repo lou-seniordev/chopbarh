580b9be73a59f1728973d8aab9f03a24e21ff7bd
topskilldev <me@topskill.dev>
2019-12-19 17:14:40 +0700

    Added new logic to Deposits for Agents

--- src/components/Dashboard/UserDeposit/DepositSmallScreens/DepositSmallScreens.js
+++ src/components/Dashboard/UserDeposit/DepositSmallScreens/DepositSmallScreens.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   Accordion,
   AccordionItem,
@@ -6,12 +6,14 @@ import {
   AccordionItemButton,
   AccordionItemPanel
 } from "react-accessible-accordion";
+import { connect } from "react-redux";
+import { Spinner } from "reactstrap";
 import styled from "styled-components";
 import Card from "../Card/Card";
 import color from "../../../styles/colors";
 import BankCharge from "../BankCharge/BankCharge";
 import Quickteller from "../Quickteller/Quickteller";
-// import Voucher from "../../UserHome/Voucher/Voucher";
+import Voucher from "../../UserHome/Voucher/Voucher";
 import GTBank from "../GTBank/GTBank";
 import NIBBS from "../NIBBS/NIBBS";
 import ATM from "../ATM/ATM";
@@ -30,108 +32,127 @@ const InstaLink = styled.a`
   }
 `;
 
-export default function DepositSmallScreens() {
-  return (
-    <div className="container p-0 my-5 px-1">
-      <div className="col-lg-12 p-0">
-        <Accordion>
-          <AccordionItem>
-            <AccordionItemHeading>
-              <AccordionItemButton>
-                Bank Transfer - All Banks (Recommended and Instant)
-              </AccordionItemButton>
-            </AccordionItemHeading>
-            <AccordionItemPanel>
-              <InstantPayment />
-            </AccordionItemPanel>
-          </AccordionItem>
+class DepositSmallScreens extends Component {
+  render() {
+    return (
+      <div className="container p-0 my-5 px-1">
+        <div className="col-lg-12 p-0">
+          {this.props.loading ? (
+            <div className="text-center">
+              <Spinner />
+            </div>
+          ) : (
+            <Accordion>
+              {this.props.playerData.PlayerStatus === 2 && (
+                <AccordionItem>
+                  <AccordionItemHeading>
+                    <AccordionItemButton>Voucher</AccordionItemButton>
+                  </AccordionItemHeading>
+                  <AccordionItemPanel>
+                    <Voucher center="true" noHeader />
+                    <div className="text-center mt-3" style={{ color: "#000" }}>
+                      <p>
+                        **Get Vouchers from{" "}
+                        <InstaLink
+                          href="https://instagram.com/chopbarhvouchers?igshid=up50qse7x2t9"
+                          style={{ color: "#ffffff" }}
+                        >
+                          @chopbarhvouchers
+                        </InstaLink>{" "}
+                        on Instagram**
+                      </p>
+                    </div>
+                  </AccordionItemPanel>
+                </AccordionItem>
+              )}
+              {this.props.playerData.PlayerStatus !== 2 && (
+                <>
+                  <AccordionItem>
+                    <AccordionItemHeading>
+                      <AccordionItemButton>
+                        Bank Transfer - All Banks (Recommended and Instant)
+                      </AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+                      <InstantPayment />
+                    </AccordionItemPanel>
+                  </AccordionItem>
 
-          <AccordionItem>
-            <AccordionItemHeading>
-              <AccordionItemButton>Bank Card</AccordionItemButton>
-            </AccordionItemHeading>
-            <AccordionItemPanel>
-              <RavePayment />
-            </AccordionItemPanel>
-          </AccordionItem>
+                  <AccordionItem>
+                    <AccordionItemHeading>
+                      <AccordionItemButton>Bank Card</AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+                      <RavePayment />
+                    </AccordionItemPanel>
+                  </AccordionItem>
 
-          <AccordionItem>
-            <AccordionItemHeading>
-              <AccordionItemButton>GT Bank</AccordionItemButton>
-            </AccordionItemHeading>
-            <AccordionItemPanel>
-              <GTBank />
-            </AccordionItemPanel>
-          </AccordionItem>
+                  <AccordionItem>
+                    <AccordionItemHeading>
+                      <AccordionItemButton>GT Bank</AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+                      <GTBank />
+                    </AccordionItemPanel>
+                  </AccordionItem>
 
-          <AccordionItem>
-            <AccordionItemHeading>
-              <AccordionItemButton>Quickteller</AccordionItemButton>
-            </AccordionItemHeading>
-            <AccordionItemPanel>
-              <Quickteller />
-            </AccordionItemPanel>
-          </AccordionItem>
+                  <AccordionItem>
+                    <AccordionItemHeading>
+                      <AccordionItemButton>Quickteller</AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+                      <Quickteller />
+                    </AccordionItemPanel>
+                  </AccordionItem>
+                  <AccordionItem>
+                    <AccordionItemHeading>
+                      <AccordionItemButton>Paystack</AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+                      <Card />
+                    </AccordionItemPanel>
+                  </AccordionItem>
 
-          {/* <AccordionItem>
-						<AccordionItemHeading>
-							<AccordionItemButton>Voucher</AccordionItemButton>
-						</AccordionItemHeading>
-						<AccordionItemPanel>
-							<Voucher center="true" noHeader />
-							<div className="text-center mt-3" style={{ color: "#000" }}>
-								<p>
-									**Get Vouchers from{" "}
-									<InstaLink
-										href="https://instagram.com/chopbarhvouchers?igshid=up50qse7x2t9"
-										style={{ color: "#ffffff" }}
-									>
-										@chopbarhvouchers
-									</InstaLink>{" "}
-									on Instagram**
-								</p>
-							</div>
-						</AccordionItemPanel>
-					</AccordionItem> */}
+                  <AccordionItem>
+                    <AccordionItemHeading>
+                      <AccordionItemButton>
+                        Zenith, UBA, Sterling, ALAT by Wema
+                      </AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+                      <BankCharge />
+                    </AccordionItemPanel>
+                  </AccordionItem>
+                  <AccordionItem>
+                    <AccordionItemHeading>
+                      <AccordionItemButton>ATM Machine</AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+                      <ATM />
+                    </AccordionItemPanel>
+                  </AccordionItem>
 
-          <AccordionItem>
-            <AccordionItemHeading>
-              <AccordionItemButton>Paystack</AccordionItemButton>
-            </AccordionItemHeading>
-            <AccordionItemPanel>
-              <Card />
-            </AccordionItemPanel>
-          </AccordionItem>
-
-          <AccordionItem>
-            <AccordionItemHeading>
-              <AccordionItemButton>
-                Zenith, UBA, Sterling, ALAT by Wema
-              </AccordionItemButton>
-            </AccordionItemHeading>
-            <AccordionItemPanel>
-              <BankCharge />
-            </AccordionItemPanel>
-          </AccordionItem>
-          <AccordionItem>
-            <AccordionItemHeading>
-              <AccordionItemButton>ATM Machine</AccordionItemButton>
-            </AccordionItemHeading>
-            <AccordionItemPanel>
-              <ATM />
-            </AccordionItemPanel>
-          </AccordionItem>
-
-          <AccordionItem>
-            <AccordionItemHeading>
-              <AccordionItemButton>NIBSS Ebillspay</AccordionItemButton>
-            </AccordionItemHeading>
-            <AccordionItemPanel>
-              <NIBBS />
-            </AccordionItemPanel>
-          </AccordionItem>
-        </Accordion>
+                  <AccordionItem>
+                    <AccordionItemHeading>
+                      <AccordionItemButton>NIBSS Ebillspay</AccordionItemButton>
+                    </AccordionItemHeading>
+                    <AccordionItemPanel>
+                      <NIBBS />
+                    </AccordionItemPanel>
+                  </AccordionItem>
+                </>
+              )}
+            </Accordion>
+          )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  playerData: state.player.playerData,
+  loading: state.player.loading
+});
+
+export default connect(mapStateToProps)(DepositSmallScreens);
