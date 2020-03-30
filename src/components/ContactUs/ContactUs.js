c1dfbc3eab799827793ee0e6d53cc601e617a67c
topskilldev <me@topskill.dev>
2020-03-30 10:30:57 +0700

    Added base logic to ContactUs component

---
+++ src/components/ContactUs/ContactUs.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import Header from "../UI/Header/Header";
+import Footer from "../UI/Footer/Footer";
+
+export default function Games() {
+  return (
+    <>
+      <Helmet title={`Chopbarh \u{2192} Contact Us`} />
+      <Header />
+
+      <Footer />
+    </>
+  );
+}
