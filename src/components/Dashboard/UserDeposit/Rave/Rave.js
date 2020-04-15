90d6b5da2cd5ee3ab1eedf1d90c8ddfd11990f7b
topskilldev <me@topskill.dev>
2020-04-15 12:14:42 +0700

    Updated reference utility function

--- src/components/Dashboard/UserDeposit/Rave/Rave.js
+++ src/components/Dashboard/UserDeposit/Rave/Rave.js
@@ -55,7 +55,7 @@ class RavePayment extends Component {
     let text = "";
     let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-    for (let i = 0; i < 10; i++)
+    for (let i = 0; i < 25; i++)
       text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return text;
--- src/lib/getReference.js
+++ src/lib/getReference.js
@@ -1,8 +1,7 @@
 export function getReference() {
   let text = "";
-  let possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  for (let i = 0; i < 15; i++)
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  for (let i = 0; i < 25; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   return text;
 }
